79707db74e35a7e3571651f1ee0ffe8bb0804be1
JeodC <47716344+JeodC@users.noreply.github.com>
2023-12-12 18:05:18 -0500

    Add cheats to Descent 1 & 2 ports
    Adds a cheats.txt file to each port and updates the .sh files to use them.

--- descent2/Descent 2.sh
+++ descent2/Descent 2.sh
@@ -25,13 +25,23 @@ ln -sfv $GAMEDIR/conf/.$GAME ~/
 export LIBGL_FB=4
 export LD_LIBRARY_PATH=$GAMEDIR/libs:/usr/libs
 
-# Edit .cfg file
+# Add some cheats
+if [ ! -f "./cheats.txt" ]; then
+	echo "Error: Cheats file not found. No cheats will be used." > /dev/tty0
+else
+	CHEATS=$(sed -n -E '/^[^#]*=[[:space:]]*1([^0-9#]|$)/s/(=[[:space:]]*1[^0-9#]*)//p' ./cheats.txt | tr -d '\n')
+fi
+
+export TEXTINPUTPRESET=$CHEATS
+
+# Edit .cfg file to correct resolution
 sed -i "s/^ResolutionX=640/ResolutionX=$DISPLAY_WIDTH/g" $GAMEDIR/conf/.$GAME/descent.cfg
 sed -i "s/^ResolutionY=480/ResolutionY=$DISPLAY_HEIGHT/g" $GAMEDIR/conf/.$GAME/descent.cfg
 
+# Run the game
 $ESUDO chmod 666 /dev/tty1
 $ESUDO chmod 666 /dev/uinput
-$GPTOKEYB "$GAME" -c "./descent2.gptk" & ./$GAME -hogdir Data 2>&1 | tee -a ./log.txt
+$GPTOKEYB "$GAME" -c "./descent2.gptk" & ./$GAME -hogdir data 2>&1 | tee -a ./log.txt
 $ESUDO kill -9 $(pidof gptokeyb)
 $ESUDO systemctl restart oga_events & 
 printf "\033c" >> /dev/tty1
