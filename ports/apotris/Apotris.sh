1e1f8ee86fccb7d18a3fcd4367f55bf020200976
Cebion <none_ssj9@hotmail.com>
2024-02-03 17:56:31 +0100

    New Port: Apotris

---
+++ ports/apotris/Apotris.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# PORTMASTER: apotris.zip, Apotris.sh
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+
+get_controls
+
+GAMEDIR=/$directory/ports/apotris
+
+exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+cd $GAMEDIR
+
+$ESUDO chmod 666 /dev/uinput
+$GPTOKEYB "apotris" &
+SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" ./apotris
+
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty0
