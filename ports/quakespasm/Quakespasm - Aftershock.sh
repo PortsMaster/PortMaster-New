3e68195a6fa8107b4deba29815075f230695ae6f
Slayer366 <andykratz@hotmail.com>
2024-02-10 17:14:56 -0600

    Updated scripts, Change controls to address drift, removed libs that interfered with ogg playback on JelOS

--- ports/quakespasm/Quakespasm - Aftershock.sh
+++ ports/quakespasm/Quakespasm - Aftershock.sh
@@ -8,7 +8,8 @@ else
   controlfolder="/roms/ports/PortMaster"
 fi
 
-source "$controlfolder/control.txt"
+source $controlfolder/control.txt
+source $controlfolder/device_info.txt
 
 get_controls
 
@@ -16,6 +17,12 @@ GAMEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/quake
 
 # Load directly into an expansion, a map, or a mod
 RUNMOD="-game as"
+
+if [ $DEVICE_NAME == 'x55' ] || [ $DEVICE_NAME == 'RG353P' ]; then
+    GPTOKEYB_CONFIG="$GAMEDIR/quakespasmtriggers.gptk"  
+else
+    GPTOKEYB_CONFIG="$GAMEDIR/quakespasm.gptk"
+fi
 
 cd $GAMEDIR
 
@@ -25,7 +32,7 @@ $ESUDO chmod 666 /dev/uinput
 
 # System
 export LD_LIBRARY_PATH=$GAMEDIR/gl4es:$GAMEDIR/libs
-
+$ESUDO rm -f $GAMEDIR/libs/libasound.so.2 $GAMEDIR/libs/libvorbis.so.0 $GAMEDIR/libs/libvorbisenc.so.2 $GAMEDIR/libs/libvorbisfile.so.3
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
 # gl4es
@@ -34,15 +41,8 @@ export LIBGL_GL=15
 
 $ESUDO rm $GAMEDIR/log.txt
 
-printf "\033c" > /dev/tty0
-echo " " > /dev/tty0
-echo "Press 'L2' to QuickSave" > /dev/tty0
-echo "Press 'R2' to QuickLoad" > /dev/tty0
-echo " " > /dev/tty0
-sleep 2
-
-$GPTOKEYB "quakespasm" -c "$GAMEDIR/quakespasm.gptk" &
-./quakespasm -current +scr_showfps 1 +joy_enable 1 +r_oldwater 1 +r_particles 1 +r_shadows 0 +r_sky_quality 6 $RUNMOD 2>&1 | tee $GAMEDIR/log.txt
+$GPTOKEYB "quakespasm" -c "$GPTOKEYB_CONFIG" &
+./quakespasm -current +scr_showfps 1 +joy_enable 0 +r_oldwater 1 +r_particles 1 +r_shadows 0 +r_sky_quality 6 $RUNMOD 2>&1 | tee $GAMEDIR/log.txt
 
 $ESUDO kill -9 $(pidof gptokeyb)
 $ESUDO systemctl restart oga_events
