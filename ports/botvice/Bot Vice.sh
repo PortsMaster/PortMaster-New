5f113f23bbad010c84e539a09a88c36530e6dd18
Fraxinus88 <157858080+Fraxinus88@users.noreply.github.com>
2024-02-12 10:10:04 +0800

    Update Bot Vice.sh

--- ports/botvice/Bot Vice.sh
+++ ports/botvice/Bot Vice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # PORTMASTER: botvice.zip, Bot Vice.sh
-# Below we assign the source of the control folder (which is the PortMaster folder) based on the distro:
+
 if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
@@ -9,28 +9,15 @@ else
   controlfolder="/roms/ports/PortMaster"
 fi
 
-# We source the control.txt file contents here
-# The $ESUDO, $directory, $param_device and necessary 
-# Sdl configuration controller configurations will be sourced from the control.txt
 source $controlfolder/control.txt
-
-# We pull the controller configs from the get_controls function from the control.txt file here
+source $controlfolder/device_info.txt
 get_controls
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
 $ESUDO chmod 666 /dev/tty0
 
-# We check on emuelec based CFWs the OS_NAME 
-[ -f "/etc/os-release" ] && source "/etc/os-release"
+GAMEDIR="/$directory/ports/botvice"
 
-GAMEDIR=/$directory/ports/botvice
-
-if [ "$OS_NAME" == "JELOS" ]; then
-  export SPA_PLUGIN_DIR="/usr/lib32/spa-0.2"
-  export PIPEWIRE_MODULE_DIR="/usr/lib32/pipewire-0.3/"
-fi
-
-# Port specific additional libraries should be included within the port's directory in a separate subfolder named libs.
-# Prioritize the armhf libs to avoid conflicts with aarch64
 export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$GAMEDIR/utils/libs"
 export GMLOADER_DEPTH_DISABLE=1
 export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
@@ -67,7 +54,7 @@ fi
 cd $GAMEDIR
 
 # Check for file existence before trying to manipulate them:
-[ -f "./gamedata/data.unx" ] && mv gamedata/data.unx gamedata/game.droid
+[ -f "./gamedata/game.win" ] && mv gamedata/data.win gamedata/game.droid
 [ -f "./gamedata/game.unx" ] && mv gamedata/game.unx gamedata/game.droid
 
 # Make sure uinput is accessible so we can make use of the gptokeyb controls
