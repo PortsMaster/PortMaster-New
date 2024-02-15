03eff2e6ee52655d4ae8111690315773c17f710b
Cebion <none_ssj9@hotmail.com>
2024-02-15 12:06:41 +0100

    New Port: Sunset Shaped

---
+++ ports/sunsetshaped/Sunset Shaped.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Below we assign the source of the control folder (which is the PortMaster folder) based on the distro:
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+# We source the control.txt file contents here
+# The $ESUDO, $directory, $param_device and necessary 
+# Sdl configuration controller configurations will be sourced from the control.txt
+source $controlfolder/control.txt
+
+# We pull the controller configs from the get_controls function from the control.txt file here
+get_controls
+
+$ESUDO chmod 666 /dev/tty0
+
+GAMEDIR=/$directory/ports/sunsetshaped
+
+exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# Port specific additional libraries should be included within the port's directory in a separate subfolder named libs.
+# Prioritize the armhf libs to avoid conflicts with aarch64
+export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$GAMEDIR/utils/libs"
+
+cd $GAMEDIR
+
+runtime="frt_3.5.2"
+if [ ! -f "$controlfolder/libs/${runtime}.squashfs" ]; then
+  # Check for runtime if not downloaded via PM
+  if [ ! -f "$controlfolder/harbourmaster" ]; then
+    echo "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info." > /dev/tty0
+    sleep 5
+    exit 1
+  fi
+
+  $ESUDO $controlfolder/harbourmaster --quiet --no-check runtime_check "${runtime}.squashfs"
+fi
+
+# Setup Godot
+godot_dir="$HOME/godot"
+godot_file="$controlfolder/libs/${runtime}.squashfs"
+$ESUDO mkdir -p "$godot_dir"
+$ESUDO umount "$godot_file" || true
+$ESUDO mount "$godot_file" "$godot_dir"
+PATH="$godot_dir:$PATH"
+
+export FRT_NO_EXIT_SHORTCUTS=FRT_NO_EXIT_SHORTCUTS
+
+$ESUDO chmod 666 /dev/uinput
+$GPTOKEYB "$runtime" -c ./sunsetshaped.gptk &
+SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" "$runtime" --main-pack "sunsetshaped.pck" -sw 2>&1 | $ESUDO tee -a ./log.txt
+
+
+$ESUDO umount "$godot_file"
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty0
