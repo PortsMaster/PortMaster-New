323fa5a8d943938dd340d3d349e464a945e5fb8a
Jacob <kloptops@gmail.com>
2024-03-25 20:06:33 +0800

    Updated DevilutionX to 1.5.2, added armhf support.

--- ports/devilutionx/Devilutionx.sh
+++ ports/devilutionx/Devilutionx.sh
@@ -9,19 +9,27 @@ else
 fi
 
 source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+
+export PORT_32BIT="N"
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
 get_controls
 
 GAMEDIR="/$directory/ports/devilution"
 cd $GAMEDIR
 
+DEVICE_ARCH="${DEVICE_ARCH:-aarch64}"
+
 $ESUDO chmod 666 /dev/tty1
 $ESUDO chmod 666 /dev/uinput
+
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib32:$GAMEDIR/libs"
-$GPTOKEYB "devilutionx" &
-./devilutionx --config-dir $GAMEDIR --data-dir $GAMEDIR --save-dir $GAMEDIR 2>&1 | tee $GAMEDIR/log.txt
+
+$GPTOKEYB "devil.${DEVICE_ARCH}" &
+./devil.${DEVICE_ARCH} --config-dir $GAMEDIR --data-dir $GAMEDIR --save-dir $GAMEDIR 2>&1 | tee $GAMEDIR/log.txt
 $ESUDO kill -9 $(pidof gptokeyb)
+
 unset LD_LIBRARY_PATH
 unset SDL_GAMECONTROLLERCONFIG
 $ESUDO systemctl restart oga_events &
