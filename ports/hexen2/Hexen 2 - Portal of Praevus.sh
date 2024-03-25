28432a8187431770bda7f55b0ded64f9b5a7e66b
Slayer366 <andykratz@hotmail.com>
2024-03-25 00:57:11 -0500

    Update Hexen 2 scripts to accommodate JelOS

--- ports/hexen2/Hexen 2 - Portal of Praevus.sh
+++ ports/hexen2/Hexen 2 - Portal of Praevus.sh
@@ -13,15 +13,19 @@ source $controlfolder/device_info.txt
 
 get_controls
 
-GAMEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/hexen2"
+[ -f "/etc/os-release" ] && source "/etc/os-release"
 
-if [ $DEVICE_NAME == 'x55' ] || [ $DEVICE_NAME == 'RG353P' ]; then
+GAMEDIR="/$directory/ports/hexen2"
+
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+$ESUDO chmod 777 -R $GAMEDIR/*
+
+if [ "$DEVICE_NAME" == "x55" ] || [ "$DEVICE_NAME" == "RG353P" ]; then
     GPTOKEYB_CONFIG="$GAMEDIR/hexen2triggers.gptk"  
 else
     GPTOKEYB_CONFIG="$GAMEDIR/hexen2.gptk"
 fi
-
-$ESUDO chmod 777 -R $GAMEDIR/*
 
 ADDLPARAMS="-conwidth 340 -lm_2 +viewsize 120 +gl_glows 1 +gl_extra_dynamic_lights 1 +gl_missile_glows 1 +gl_other_glows 1 +gl_colored_dynamic_lights 1 +gl_coloredlight 2 +r_waterwarp 0 +showfps 0"
 
@@ -32,30 +36,34 @@ cd $GAMEDIR
 
 $ESUDO chmod 666 /dev/tty0
 $ESUDO chmod 666 /dev/tty1
-$ESUDO chmod 666 /dev/uinput
-
-# System (Also prepare runtime directories for User and Pulse Audio)
-export LD_LIBRARY_PATH=$GAMEDIR/lib:$LD_LIBRARY_PATH
-[ -f "$GAMEDIR/lib/libasound.so.2" ] && $ESUDO rm -f $GAMEDIR/lib/libasound.so.2 $GAMEDIR/lib/libvorbis.so.0 $GAMEDIR/lib/libvorbisenc.so.2 $GAMEDIR/lib/libvorbisfile.so.3
-export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
 # gl4es
 export LIBGL_GL=15
 export LIBGL_ES=1
 
-# prep for clean log
-$ESUDO rm $GAMEDIR/log.txt
+# Detect JelOS/Device combination and address GL libs accordingly.
+if [ "$OS_NAME" == "JELOS" ] && [ "$DEVICE_NAME" == "RG351P" ] || [ "$DEVICE_NAME" == "RG351M" ] || [ "$DEVICE_NAME" == "RG351MP" ] || [ "$DEVICE_NAME" == "RG351V" ]; then
+  [ -f "$GAMEDIR/libs.jelos/libEGL.so.1" ] && $ESUDO rm -rf "$GAMEDIR/libs.jelos/libEGL.so.1"
+else
+  [ ! -f "$GAMEDIR/libs.jelos/libEGL.so.1" ] && $ESUDO cp -f "$GAMEDIR/lib/libEGL.so.1" "$GAMEDIR/libs.jelos/libEGL.so.1"
+fi
+
+if [ "$OS_NAME" == "JELOS" ]; then
+  export LD_LIBRARY_PATH="$GAMEDIR/libs.jelos:/usr/lib:$LD_LIBRARY_PATH"
+else
+  export LD_LIBRARY_PATH="$GAMEDIR/lib:$LD_LIBRARY_PATH"
+fi
+
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
 # setup symbolic link to config directory
 $ESUDO rm -rf ~/.hexen2
-$ESUDO ln -s /$GAMEDIR/conf/.hexen2 ~/
+$ESUDO ln -sfv $GAMEDIR/conf/.hexen2 ~/
 
 $GPTOKEYB "glhexen2" -c "$GPTOKEYB_CONFIG" &
-./glhexen2 -width $DISPLAY_WIDTH -height $DISPLAY_HEIGHT $ADDLPARAMS -basedir ./ $RUNMOD 2>&1 | tee -a ./log.txt
+./glhexen2 -width $DISPLAY_WIDTH -height $DISPLAY_HEIGHT $ADDLPARAMS -basedir ./ $RUNMOD
 
 $ESUDO kill -9 $(pidof gptokeyb)
 $ESUDO systemctl restart oga_events &
-unset LD_LIBRARY_PATH
-unset SDL_GAMECONTROLLERCONFIG
 printf "\033c" > /dev/tty1
 printf "\033c" > /dev/tty1
