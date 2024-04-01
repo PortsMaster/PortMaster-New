67d71e33aa38c065882d4fe2bbd9e85315a83dc1
Hoang Nguyen <nkahoang@gmail.com>
2024-04-01 21:55:06 +1100

    Moving items around for RGB30

--- ports/balatro/Balatro.sh
+++ ports/balatro/Balatro.sh
@@ -12,11 +12,6 @@ source $controlfolder/device_info.txt
 get_controls
 
 GAMEDIR="/$directory/ports/balatro"
-TILE_W="s/self.TILE_W = self.F_MOBILE_UI and 11.5 or 20/self.TILE_W = 16.5/g"
-TILE_H="s/self.TILE_H = self.F_MOBILE_UI and 20 or 11.5/self.TILE_H = 16.5/g"
-CRT="s/crt = 70,/crt = 0,/g"
-SHADOWS="s/shadows = 'On'/shadows = 'Off'/g"
-BLOOM="s/bloom = 1/bloom = 0/g"
 
 export XDG_DATA_HOME="$GAMEDIR/saves"
 export XDG_CONFIG_HOME="$GAMEDIR/saves"
@@ -40,16 +35,38 @@ if [ -f "$GAMEFILE" ]; then
   ./bin/7za x "$GAMEFILE" globals.lua
 
   # Modify globals.lua
-  sed -i "$CRT" -i "$SHADOWS" -i "$BLOOM" globals.lua
 
-  if [ $DISPLAY_WIDTH -le 1279 ]; then # increase the scale
-      sed -i "$TILE_W" -i "$TILE_H" globals.lua
+  # change some default settings
+  sed -i 's/crt = 70,/crt = 0,/g' -i 's/bloom = 1/bloom = 0/g' globals.lua
+  sed -i 's/s/shadows = 'On'/shadows = 'Off'/g' globals.lua
+  sed -i 's/self.F_HIDE_BG = false/self.F_HIDE_BG = true/g' globals.lua
+
+  if [ $DISPLAY_WIDTH -le 1279 ]; then # increase the scale for smaller screens
+    sed -i 's/self.TILE_W = self.F_MOBILE_UI and 11.5 or 20/self.TILE_W = 18.25/g' globals.lua
+    sed -i 's/self.TILE_H = self.F_MOBILE_UI and 20 or 11.5/self.TILE_H = 18.25/g' globals.lua
   fi
 
   if [ $DISPLAY_WIDTH -le 720 ]; then # switch out the font if the screen is too small; helping with readability
     cp resources/fonts/Nunito-Black.ttf resources/fonts/m6x11plus.ttf # change Nunito-Black to the in-game font file
     ./bin/7za u -aoa "$GAMEFILE" resources/fonts/m6x11plus.ttf
     rm resources/fonts/m6x11plus.ttf
+
+    if [ $DISPLAY_HEIGHT -eq 720 ]; then # RGB30 specific changes
+      mkdir -p ./functions
+      ./bin/7za x "$GAMEFILE" functions/common_events.lua
+      # move the hands a bit to the right
+      sed -i 's/G.hand.T.x = G.TILE_W - G.hand.T.w - 2.85/G.hand.T.x = G.TILE_W - G.hand.T.w - 1/g' functions/common_events.lua
+      # then move the playing area up
+      sed -i 's/G.play.T.y = G.hand.T.y - 3.6/G.play.T.y = G.hand.T.y - 4.5/g' functions/common_events.lua
+      # move the decks to the right
+      sed -i 's/G.deck.T.x = G.TILE_W - G.deck.T.w - 0.5/G.deck.T.x = G.TILE_W - G.deck.T.w + 0.85/g' functions/common_events.lua
+      # move the jokers to the left
+      sed -i 's/G.jokers.T.x = G.hand.T.x - 0.1/G.jokers.T.x = G.hand.T.x - 0.15/g' functions/common_events.lua
+
+      # Update the archive with the modified common_events.lua
+      ./bin/7za u -aoa "$GAMEFILE" functions/common_events.lua
+      rm functions/common_events.lua
+    fi
   fi
 
   # Update the archive with the modified globals.lua
