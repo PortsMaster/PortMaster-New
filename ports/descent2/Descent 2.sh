ea51db3bd551cd3568c80f635f31a6f8e695606d
JeodC <47716344+JeodC@users.noreply.github.com>
2024-04-03 16:18:34 -0400

    The big update

--- ports/descent2/Descent 2.sh
+++ ports/descent2/Descent 2.sh
@@ -13,23 +13,28 @@ else
 fi
 
 source $controlfolder/control.txt
-
+source $controlfolder/device_info.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 get_controls
 
-source $controlfolder/device_info.txt
-
-GAME="d2x-rebirth"
 GAMEDIR="/$directory/ports/descent2"
+DEVICE_ARCH="${DEVICE_ARCH:-aarch64}"
+GAME="d2x-rebirth"
 
 cd $GAMEDIR
 
-$ESUDO rm -rf ~/.$GAME
-ln -sfv $GAMEDIR/conf/.$GAME ~/
+if [ -f "${controlfolder}/libgl_${CFW_NAME}.txt" ]; then 
+  source "${controlfolder}/libgl_${CFW_NAME}.txt"
+else
+  source "${controlfolder}/libgl_default.txt"
+fi
 
-export LIBGL_FB=4
-export LD_LIBRARY_PATH=$GAMEDIR/libs:/usr/lib
+$ESUDO rm -rf ~/.$GAME
+ln -sfv $GAMEDIR/config ~/.$GAME
+
+export LD_LIBRARY_PATH="$GAMEDIR/libs.$DEVICE_ARCH:$LD_LIBRARY_PATH"
 export SDL_FORCE_SOUNDFONTS=1
-export SDL_SOUNDFONTS="$GAMEDIR/libs/soundfont.sf2"
+export SDL_SOUNDFONTS="$GAMEDIR/soundfont.sf2"
 
 # Add some cheats
 if [ ! -f "./cheats.txt" ]; then
@@ -40,19 +45,26 @@ fi
 
 export TEXTINPUTPRESET=$CHEATS
 
-# Edit .cfg file to correct resolution
-sed -i "s/^ResolutionX=640/ResolutionX=$DISPLAY_WIDTH/g" $GAMEDIR/conf/.$GAME/descent.cfg
-sed -i "s/^ResolutionY=480/ResolutionY=$DISPLAY_HEIGHT/g" $GAMEDIR/conf/.$GAME/descent.cfg
+# Edit .cfg file with updated resolution and aspect ratio
+sed -i "s/^ResolutionX=640/ResolutionX=$DISPLAY_WIDTH/g" $GAMEDIR/config/descent.cfg
+sed -i "s/^ResolutionY=480/ResolutionY=$DISPLAY_HEIGHT/g" $GAMEDIR/config/descent.cfg
+sed -i "s/^AspectX=.*/AspectX=$ASPECT_Y/g" $GAMEDIR/config/descent.cfg
+sed -i "s/^AspectY=.*/AspectY=$ASPECT_X/g" $GAMEDIR/config/descent.cfg
 
 # Setup controls
 $ESUDO chmod 666 /dev/tty1
 $ESUDO chmod 666 /dev/uinput
-$GPTOKEYB "$GAME" -c "conf/joy.gptk" & 
-SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
-# Run the game
-./$GAME -hogdir data
+if [ $CFW_NAME == "ArkOS" ]; then
+	$GPTOKEYB "$GAME.compat" -c "config/joy.gptk" & 
+	SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+	./$GAME.compat -hogdir data
+else
+	$GPTOKEYB "$GAME.$DEVICE_ARCH" -c "config/joy.gptk" & 
+	SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+	./$GAME.$DEVICE_ARCH -hogdir data
+fi
+
 $ESUDO kill -9 $(pidof gptokeyb)
 $ESUDO systemctl restart oga_events & 
 printf "\033c" >> /dev/tty1
-
