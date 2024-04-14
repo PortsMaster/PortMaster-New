8addc85885977e1015161e6c63a527078cd6d3cd
Captain-Barge <ThirdParachute@protonmail.com>
2024-04-14 15:21:49 -0700

    Reworks with Slayer366 and Cebion

--- ports/minetest/Minetest.sh
+++ ports/minetest/Minetest.sh
@@ -13,70 +13,31 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/tasksetter
+source $controlfolder/device_info.txt
 
 get_controls
 
-CUR_TTY=/dev/tty0
+GAMEDIR="/$directory/ports/minetest"
 
-PORTDIR="/$directory/ports/"
-GAMEDIR="${PORTDIR}/minetest"
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
 cd $GAMEDIR
 
-# Grab text output...
-$ESUDO chmod 666 $CUR_TTY
-"$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-export TERM=linux
-printf "\033c" > $CUR_TTY
-
-# Remove libcurl.so.4 if it already exists in system libraries
-FILE_TO_CHECK="libcurl.so.4" # Example library file name
-EXTRA_LIB_FOLDER="libs.aarch64"
-FILE_TO_DELETE="$GAMEDIR/$EXTRA_LIB_FOLDER/$FILE_TO_CHECK"
-
-# Convert LD_LIBRARY_PATH into an array of directories
-IFS=':' read -ra DIRS <<< "$LD_LIBRARY_PATH"
-
-for dir in "${DIRS[@]}"; do  # Iterate over dirs in LD_LIBRARY_PATH
-    full_path="$dir/$FILE_TO_CHECK"
-    if [ -f "$full_path" ]; then  # If file exists locally...
-        if [ -f "$FILE_TO_DELETE" ]; then  #...and it exists in bundle
-            rm "$FILE_TO_DELETE"  # ...then delete it in bundle
-        fi
-        break
-    fi
-done
-
-export LD_LIBRARY_PATH="$GAMEDIR/$EXTRA_LIB_FOLDER:$LD_LIBRARY_PATH"
-
+$ESUDO chmod 666 /dev/tty0
 $ESUDO chmod 666 /dev/tty1
 $ESUDO chmod 666 /dev/uinput
+
+export TERM=linux
+printf "\033c" > /dev/tty0
+
+export DEVICE_ARCH="${DEVICE_ARCH:-aarch64}"
+export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
-# Load control config appropriate to available joysticks
-if [ "$ANALOG_STICKS" = "0" ]; then
-    GPTK_FILE="minetest_0stick.gptk"
-    # Turn on autojump
-    # if ! grep -Fxq "autojump" "$GAMEDIR/minetest.conf"; then
-    #     echo 'autojump = true' | sudo tee -a "$GAMEDIR/minetest.conf"
-    # fi
-elif [ "$ANALOG_STICKS" = "1" ]; then
-    GPTK_FILE="minetest_1stick.gptk"
-    # Turn on autojump
-    # if ! grep -Fxq "autojump" "$GAMEDIR/minetest.conf"; then
-    #     echo 'autojump = true' | sudo tee -a "$GAMEDIR/minetest.conf"
-    # fi
-else
-    GPTK_FILE="minetest_2stick.gptk"
-fi
-
-$GPTOKEYB "./bin/minetest" -c "$GAMEDIR/$GPTK_FILE" &
-$TASKSET ./bin/minetest
+$GPTOKEYB "minetest" -c "$GAMEDIR/minetest.gptk.$ANALOG_STICKS" &
+./bin/minetest
 
 $ESUDO kill -9 $(pidof gptokeyb)
-unset LD_LIBRARY_PATH
-unset SDL_GAMECONTROLLERCONFIG
 $ESUDO systemctl restart oga_events &
-
-# Disable console
-printf "\033c" >> $CUR_TTY
+printf "\033c" > /dev/tty1
+printf "\033c" > /dev/tty0
