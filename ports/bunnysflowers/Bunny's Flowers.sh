b364ccb209c1ce3b5c84f1d5fe534985239013da
Maarten Hoogcarspel <maarten@hoogcarspel.net>
2024-04-23 11:46:30 +0100

    add xdelta3 detection to sh, arch to json, smallify screenshot

--- ports/bunnysflowers/Bunny's Flowers.sh
+++ ports/bunnysflowers/Bunny's Flowers.sh
@@ -20,6 +20,13 @@ DROID="game.droid"
 
 exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
+# check if we have new enough version of PortMaster that contains xdelta3
+if [ ! -f "$controlfolder/xdelta3" ]; then
+  echo "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info." > /dev/tty0
+  sleep 5
+  exit 1
+fi
+
 # patch is based on steam app/depot/build/manifest 1375480 1375482 6478677 7594562866968444214 to enable autosave
 if [ -f "./gamedata/data.win" ] && md5sum -c hash.md5 >/dev/null 2>&1; then
   ${controlfolder}/xdelta3 -d -s "$GAMEDIR/gamedata/data.win" "$GAMEDIR/bunnysflowers.patch" "$GAMEDIR/gamedata/$DROID" && \
