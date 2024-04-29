e7300be20b48bc44bafdf30763a48b23beb9493a
Blackerererer <116157599+Blackerererer@users.noreply.github.com>
2024-04-29 11:26:43 +0200

    Flare amhf. Multiarch support in sh. Cleaned up readmes.

--- ports/flare/Flare Engine.sh
+++ ports/flare/Flare Engine.sh
@@ -13,20 +13,29 @@ else
 fi
 
 source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
 get_controls
 
 GAMEDIR=/$directory/ports/flare
 cd $GAMEDIR
 
+CONFDIR="$GAMEDIR/flare"
+mkdir -p "$CONFDIR"
+export XDG_DATA_HOME="$CONFDIR"
+
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+DEVICE_ARCH="${DEVICE_ARCH:-aarch64}"
+
 $ESUDO rm -rf ~/.config/flare
 ln -sfv /$directory/ports/flare/conf/.config/flare ~/.config/
 
-
 $ESUDO chmod 666 /dev/uinput
-$GPTOKEYB "flare" xbox360 &
-LD_LIBRARY_PATH="$PWD/libs" SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" ./flare 2>&1 | tee -a ./log.txt
+$GPTOKEYB "flare.$DEVICE_ARCH" xbox360 &
+SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" ./flare.$DEVICE_ARCH
 $ESUDO kill -9 $(pidof gptokeyb)
 $ESUDO systemctl restart oga_events &
 printf "\033c" > /dev/tty0
-
