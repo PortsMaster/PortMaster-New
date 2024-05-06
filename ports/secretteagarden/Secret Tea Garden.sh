2a6f547a4960f3599c1346a55048385990234508
MadShmupper <149628836+MadShmupper@users.noreply.github.com>
2024-05-05 21:16:00 -0700

    Update Secret Tea Garden.sh
    took out unneeded game title in the .gptk sequence

--- ports/secretteagarden/Secret Tea Garden.sh
+++ ports/secretteagarden/Secret Tea Garden.sh
@@ -47,7 +47,7 @@ fi
 # Make sure uinput is accessible so we can make use of the gptokeyb controls
 $ESUDO chmod 666 /dev/uinput
 
-$GPTOKEYB "gmloader" -c ./secretgarden.gptk &
+$GPTOKEYB "gmloader" -c ./.gptk &
 
 $ESUDO chmod +x "$GAMEDIR/gmloader"
 
