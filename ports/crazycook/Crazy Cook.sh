774c7125ec4143816d2fb8636f3cde6156753a74
tabreturn <github@tabreturn.com>
2024-05-10 14:30:27 +1000

    - update header and game path

--- ports/crazycook/Crazy Cook.sh
+++ ports/crazycook/Crazy Cook.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
 if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
   controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
 else
   controlfolder="/roms/ports/PortMaster"
 fi
@@ -15,7 +19,7 @@ source $controlfolder/device_info.txt
 
 get_controls
 
-GAMEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/crazycook"
+GAMEDIR=/$directory/ports/crazycook
 CONFDIR="$GAMEDIR/conf/"
 
 # Ensure the conf directory exists
