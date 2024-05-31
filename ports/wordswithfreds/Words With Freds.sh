1f1775286bb3efa2ce98b2a73493a46d27a49546
Fraxinus88 <157858080+Fraxinus88@users.noreply.github.com>
2024-05-31 18:44:18 +0800

    New Port Words With Freds

---
+++ ports/wordswithfreds/Words With Freds.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+export PORT_32BIT="Y"
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+$ESUDO chmod 666 /dev/tty0
+
+GAMEDIR=/$directory/ports/wordswithfreds
+
+exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$GAMEDIR/utils/libs"
+export GMLOADER_DEPTH_DISABLE=1
+export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
+
+cd $GAMEDIR
+
+#Patch Game
+if [ -f "$GAMEDIR/game.7z" ]; then
+   
+    # Change directory to the specified directory
+    cd "$GAMEDIR" || exit
+
+    # Use 7zip to extract the .exe file to the destination directory
+    "$GAMEDIR/patch/7zzs" x "$GAMEDIR/game.7z" -o"$GAMEDIR/gamedata" & pid=$!
+
+    # Wait for the extraction process to complete
+    wait $pid
+
+    # Check if the panicroom.exe file exists
+    if [ -f "$GAMEDIR/gamedata/game.droid" ]; then
+        # Delete the redundant .exe files
+	rm "$GAMEDIR/game.7z"
+    fi
+else
+    echo "Game has been patched!"
+fi
+
+$ESUDO chmod 666 /dev/uinput
+
+$GPTOKEYB "gmloader" &
+
+$ESUDO chmod +x "$GAMEDIR/gmloader"
+
+./gmloader wordswithfreds.apk
+
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty0
