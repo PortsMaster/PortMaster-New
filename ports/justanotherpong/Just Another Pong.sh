647460fcf3cb43d6dec7eee78825123c208863c0
JanTrueno <159560609+JanTrueno@users.noreply.github.com>
2024-06-12 14:22:05 +0200

    Add portfiles for Just Another Pong

---
+++ ports/justanotherpong/Just Another Pong.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+
+get_controls
+
+GAMEDIR=/$directory/ports/justanotherpong/
+CONFDIR="$GAMEDIR/conf/"
+
+# Ensure the conf directory exists
+mkdir -p "$GAMEDIR/conf"
+
+# Set the XDG environment variables for config & savefiles
+export XDG_CONFIG_HOME="$CONFDIR"
+export XDG_DATA_HOME="$CONFDIR"
+
+cd $GAMEDIR
+
+$ESUDO chmod 666 /dev/uinput
+
+$GPTOKEYB "love" -c "./justanotherpong.gptk" &
+LD_LIBRARY_PATH="$PWD/libs" SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" ./love gamedata/just-another-pong.love 2>&1 | tee $GAMEDIR/log.txt
+
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty0
---
+++ ports/justanotherpong/README.md
@@ -0,0 +1,14 @@
+## Notes
+
+A huge thanks goes out to [Hamdy Elzanqali/CYFO](https://cyfo.itch.io/) for creating this awesome little game and giving PortMaster the permission to distribute for free!
+
+## Controls
+
+| Button | Action |
+|--|--| 
+|D-pad|Player One|
+|X/B|Player Two|
+|A|Select|
+|Start|Exit|
+
+
