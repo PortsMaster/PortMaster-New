3e9c70981f736ab384ca36b37e8bfc307800533d
JanTrueno <janhblokstra@gmail.com>
2024-06-12 20:00:40 +0200

    Update port to new standards.

--- ports/justanotherpong/Just Another Pong.sh
+++ ports/justanotherpong/Just Another Pong.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
 if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
   controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
 else
   controlfolder="/roms/ports/PortMaster"
 fi
 
 source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
 get_controls
 
-GAMEDIR=/$directory/ports/justanotherpong/
+GAMEDIR=/$directory/ports/justanotherpong
 CONFDIR="$GAMEDIR/conf/"
 
-# Ensure the conf directory exists
 mkdir -p "$GAMEDIR/conf"
-
-# Set the XDG environment variables for config & savefiles
-export XDG_CONFIG_HOME="$CONFDIR"
-export XDG_DATA_HOME="$CONFDIR"
-
 cd $GAMEDIR
 
-$ESUDO chmod 666 /dev/uinput
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-$GPTOKEYB "love" -c "./justanotherpong.gptk" &
-LD_LIBRARY_PATH="$PWD/libs" SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" ./love gamedata/just-another-pong.love 2>&1 | tee $GAMEDIR/log.txt
+# Set the XDG environment variables for config & savefiles
+export XDG_DATA_HOME="$CONFDIR"
+export DEVICE_ARCH="${DEVICE_ARCH:-aarch64}"
+export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+$GPTOKEYB "love.${DEVICE_ARCH}" -c "./justanotherpong.gptk" &
+./love.${DEVICE_ARCH} gamedata/just-another-pong.love
 
 $ESUDO kill -9 $(pidof gptokeyb)
 $ESUDO systemctl restart oga_events &
 printf "\033c" > /dev/tty0
+printf "\033c" > /dev/tty1
