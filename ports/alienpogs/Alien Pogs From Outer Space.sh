d3838b00eb3818cae53bbf8d1e301146afbaadc8
Tlaggypyro <154095929+Tlaggypyro@users.noreply.github.com>
2024-07-12 20:42:16 -0300

    add alien pog files

---
+++ ports/alienpogs/Alien Pogs From Outer Space.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+
+get_controls
+
+GAMEDIR=/$directory/ports/alienpogs
+CONFDIR="$GAMEDIR/conf/"
+DATAFILE=alienpogs.rpg
+
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+mkdir -p "$GAMEDIR/conf"
+
+export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export TEXTINPUTINTERACTIVE="Y"
+export XDG_DATA_HOME="$CONFDIR"
+
+cd $GAMEDIR
+
+[ ! -f "$GAMEDIR/alienpogs.rpg" ] && mv "$GAMEDIR/"*.rpg "$GAMEDIR/alienpogs.rpg"
+
+$GPTOKEYB "ohrrpgce-game" &
+
+"./ohrrpgce-game" $DATAFILE -f
+
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty0
