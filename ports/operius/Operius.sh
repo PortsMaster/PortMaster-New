456de13a35b186a6cce8a5b5bebb3c00cdf8044a
Fraxinus88 <157858080+Fraxinus88@users.noreply.github.com>
2024-08-09 23:05:08 +0800

    Update Operius.sh

--- ports/operius/Operius.sh
+++ ports/operius/Operius.sh
@@ -25,7 +25,7 @@ GAMEDIR=/$directory/ports/operius
 
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$GAMEDIR/utils/libs"
+export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$GAMEDIR/utils/libs:$LD_LIBRARY_PATH"
 export GMLOADER_DEPTH_DISABLE=1
 export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
 export GMLOADER_PLATFORM="os_windows"
