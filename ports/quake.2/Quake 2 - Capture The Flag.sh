c9961202ae2e580897196eeeb9f34739a66cdf49
Slayer366 <andykratz@hotmail.com>
2024-10-04 18:21:37 -0500

    Update Quake II, add support for GLES and expansion packs

---
+++ ports/quake.2/Quake 2 - Capture The Flag.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+to_lower_case() {
+    find "$1" -depth \( -name "*.PAK" -o -name "PAK0.pak" \) | while IFS= read -r SRC; do
+        DST=$(dirname "$SRC")/$(basename "$SRC" | tr '[:upper:]' '[:lower:]')
+        TMP_DST=$(dirname "$SRC")/temp_$(basename "$SRC" | tr '[:upper:]' '[:lower:]')
+        echo "SRC: $SRC"
+        echo "DST: $DST"
+        echo "TMP_DST: $TMP_DST"
+        if [ "$SRC" != "$DST" ]; then
+            echo "Renaming $SRC to $TMP_DST"
+            $ESUDO mv -vf "$SRC" "$TMP_DST"
+            echo "Renaming $TMP_DST to $DST"
+            $ESUDO mv -vf "$TMP_DST" "$DST"
+        else
+            echo "- $SRC is already lowercase"
+        fi
+    done
+}
+
+#GAMEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/quake2"
+GAMEDIR="/$directory/ports/quake2"
+
+$ESUDO chmod 777 -R $GAMEDIR/*
+
+cd $GAMEDIR
+
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+to_lower_case "$GAMEDIR/ctf"
+
+$ESUDO chmod 666 /dev/tty0
+$ESUDO chmod 666 /dev/tty1
+$ESUDO chmod 666 /dev/uinput
+
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+if [[ "$DISPLAY_WIDTH" == '480' ]]; then
+    SSCALE="1"
+else
+    SSCALE="3"
+fi
+
+if [[ "${CFW_NAME^^}" == 'JELOS' ]] || [[ "${CFW_NAME^^}" == 'ROCKNIX' ]]; then
+    YQ2RENDERER="soft"
+    GUNFOV="82"
+else
+    YQ2RENDERER="gles3"
+    GUNFOV="70"
+fi
+
+if [[ "${DEVICE_NAME^^}" == 'X55' ]] || [[ "${DEVICE_NAME^^}" == 'RG353P' ]] || [[ "${DEVICE_NAME^^}" == 'RG40XX' ]]; then
+      if [ ! -f $GAMEDIR/conf/.yq2/console_history.txt ]; then
+        mkdir -p $GAMEDIR/conf/.yq2
+        cp -rf $GAMEDIR/conf/yq2_triggers/* $GAMEDIR/conf/.yq2/.
+      fi
+elif [[ "${ANALOG_STICKS}" -lt 2 ]]; then
+      if [ ! -f $GAMEDIR/conf/.yq2/console_history.txt ]; then
+        mkdir -p $GAMEDIR/conf/.yq2
+        cp -rf $GAMEDIR/conf/yq2_nosticks/* $GAMEDIR/conf/.yq2/.
+      fi
+else
+      if [ ! -f $GAMEDIR/conf/.yq2/console_history.txt ]; then
+        mkdir -p $GAMEDIR/conf/.yq2
+        cp -rf $GAMEDIR/conf/yq2_default/* $GAMEDIR/conf/.yq2/.
+      fi
+fi
+
+#ADDLPARAMS="+set in_initjoy 0 +set cl_showfps 1 +set r_shadows 1 -datadir $GAMEDIR +set vid_fullscreen 2 -portable"
+$ESUDO rm -rf ~/.yq2
+$ESUDO ln -sfv /$GAMEDIR/conf/.yq2 ~/
+
+$GPTOKEYB "quake2" -c "$GAMEDIR/quake2.gptk" &
+./quake2 +set vid_renderer $YQ2RENDERER +set r_mode -2 +set r_customwidth $DISPLAY_WIDTH +set r_customheight $DISPLAY_HEIGHT +set r_gunfov $GUNFOV +set r_hudscale $SSCALE +set r_menuscale $SSCALE +set game ctf
+
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty1
+printf "\033c" > /dev/tty0
