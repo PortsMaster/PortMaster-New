2d0cf72c0489d48e9bcb156230bd429ee912c501
Steven Trevino <Steven.Trevino@rsginc.com>
2024-10-13 17:49:50 -0500

    Jeod gmloadernext & tweaks

--- ports/powerputtlegend/Powerputt Legend.sh
+++ ports/powerputtlegend/Powerputt Legend.sh
@@ -13,14 +13,8 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 get_controls
-
-# Setup permissions
-$ESUDO chmod 666 /dev/tty0
-$ESUDO chmod 666 /dev/uinput
-$ESUDO chmod +x "$GAMEDIR/gmloadernext"
 
 # Variables
 GAMEDIR="/$directory/ports/powerputtlegend"
@@ -32,12 +26,16 @@ $ESUDO chmod +x -R $GAMEDIR/*
 
 # Exports
 export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$GAMEDIR/libs:$LD_LIBRARY_PATH"
-
-$GPTOKEYB "gmloadernext" &
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+# Display loading splash
+$ESUDO ./libs/splash "splash.png" 1 
+$ESUDO ./libs/splash "splash.png" 1000
+
+# Run game
+$GPTOKEYB "gmloadernext" &
+pm_platform_helper "$GAMEDIR/gmloadernext"
 ./gmloadernext game.apk
 
 # Kill processes
-$ESUDO kill -9 $(pidof gptokeyb)
-$ESUDO systemctl restart oga_events &
-printf "\033c" > /dev/tty0
+pm_finish
