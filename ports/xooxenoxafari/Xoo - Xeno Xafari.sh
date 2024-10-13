34bfd54e43bf7618edafa1514a37c14253eee1a6
tabreturn <github@tabreturn.com>
2024-10-13 19:32:59 +1100

    - adapt .sh for p.m changes

--- ports/xooxenoxafari/Xoo - Xeno Xafari.sh
+++ ports/xooxenoxafari/Xoo - Xeno Xafari.sh
@@ -13,10 +13,7 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
-
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
-
 get_controls
 
 GAMEDIR=/$directory/ports/xooxenoxafari
@@ -26,7 +23,6 @@ DATAFILE=xeno.rpg
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
 mkdir -p "$GAMEDIR/conf"
-
 export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 export TEXTINPUTINTERACTIVE="Y"
@@ -35,8 +31,7 @@ export XDG_DATA_HOME="$CONFDIR"
 cd $GAMEDIR
 
 $GPTOKEYB "ohrrpgce-game" -c ./xooxenoxafari.gptk &
+pm_platform_helper $GAMEDIR/ohrrpgce-game
 "./ohrrpgce-game" $DATAFILE -f
 
-$ESUDO kill -9 $(pidof gptokeyb)
-$ESUDO systemctl restart oga_events &
-printf "\033c" > /dev/tty0
+pm_finish
