97184c82dca075168aedcc7060f5964b067eadf7
tabreturn <github@tabreturn.com>
2024-10-15 20:01:38 +1100

    - clean-up whitespace

--- ports/catsonmars/Cats on Mars.sh
+++ ports/catsonmars/Cats on Mars.sh
@@ -13,10 +13,7 @@ else
 fi
 
 source $controlfolder/control.txt
-# device_info.txt will be included by default
-
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
-
 get_controls
 
 GAMEDIR=/$directory/ports/catsonmars
