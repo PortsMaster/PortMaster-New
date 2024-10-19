1b6cb62a851c29fcf05103ef6ea1e2e7bc8da458
JanTrueno <janhblokstra@gmail.com>
2024-10-19 14:40:35 +0100

    Add portfiles for Garden Story

---
+++ ports/gardenstory/Garden Story.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR="/$directory/ports/gardenstory"
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# Exports
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$LD_LIBRARY_PATH"
+
+# Check if "data.win" exists and its MD5 checksum matches the specified value then apply patch
+if [ -f "gamedata/data.win" ]; then
+    checksum=$(md5sum "gamedata/data.win" | awk '{print $1}')
+    if [ "$checksum" = "ad61dfb29cda512397bf34a4aa70db8a" ]; then
+        $ESUDO $controlfolder/xdelta3 -d -s "gamedata/data.win" -f "./patch/patch.xdelta" "gamedata/game.droid" && \
+        rm "gamedata/data.win"
+    fi
+fi
+
+# Check if there are .dat files in ./gamedata
+if [ -n "$(ls ./gamedata/*.dat 2>/dev/null)" ]; then
+    # Move all .dat files from ./gamedata to ./assets
+    mkdir -p ./assets
+    mv ./gamedata/*.dat ./assets/ || exit 1
+
+    # Zip the contents of ./game.apk including the .dat files
+    zip -r -0 ./game.apk ./assets/ || exit 1
+    rm -Rf "$GAMEDIR/assets/" || exit 1
+fi
+
+# Display loading splash
+if [ -f "$GAMEDIR/patchlog.txt" ]; then
+    $ESUDO ./tools/splash "splash.png" 1 
+    $ESUDO ./tools/splash "splash.png" 2000
+fi
+
+# Run the game
+$GPTOKEYB "gmloadernext" &
+pm_platform_helper "$GAMEDIR/gmloadernext"
+./gmloadernext
+
+# Kill processes
+pm_finish
