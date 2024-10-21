6fd7d4942be47a81f203c6c4905c4c617ca06fb5
Slayer366 <andykratz@hotmail.com>
2024-10-21 11:44:27 -0500

    Adjust to emphasize and accommodate architecture

--- ports/supermethanebros/Super Methane Brothers.sh
+++ ports/supermethanebros/Super Methane Brothers.sh
@@ -19,14 +19,14 @@ get_controls
 
 GAMEDIR="/$directory/ports/supermethanebros"
 
-export LD_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
 cd "$GAMEDIR"
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-$GPTOKEYB2 "methane" -c "$GAMEDIR/methane.gptk" &
-pm_platform_helper "$GAMEDIR/methane"
-./methane
+$GPTOKEYB2 "methane.${DEVICE_ARCH}" -c "$GAMEDIR/methane.gptk" &
+pm_platform_helper "$GAMEDIR/methane.${DEVICE_ARCH}"
+./methane.${DEVICE_ARCH}
 
 pm_finish
