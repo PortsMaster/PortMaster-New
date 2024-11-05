bb4e5c60fc170f949cecb2c83662300726008c9a
Cebion <7762208+Cebion@users.noreply.github.com>
2024-11-05 16:09:22 +0100

    Remove ld_library_path

--- ports/grielsquest/Griel's Quest for the Sangraal.sh
+++ ports/grielsquest/Griel's Quest for the Sangraal.sh
@@ -26,7 +26,6 @@ BINARY=griels
 
 mkdir -p "$GAMEDIR/conf"
 
-export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 export TEXTINPUTINTERACTIVE="Y" 
 
