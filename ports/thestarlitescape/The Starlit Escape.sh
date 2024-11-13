3008bb12cd467c3987dd5d6884dfb8863595a17b
Fraxinus88 <157858080+Fraxinus88@users.noreply.github.com>
2024-11-13 22:54:49 +0800

    Update The Starlit Escape.sh

--- ports/thestarlitescape/The Starlit Escape.sh
+++ ports/thestarlitescape/The Starlit Escape.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
-
-XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
-
 if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
   controlfolder="/opt/tools/PortMaster"
-elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
-  controlfolder="$XDG_DATA_HOME/PortMaster"
 else
   controlfolder="/roms/ports/PortMaster"
 fi
 
 source $controlfolder/control.txt
-export PORT_32BIT="Y"
 
 get_controls
-[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
-GAMEDIR="/$directory/ports/thestarlitescape"
+[ -f "/etc/os-release" ] && source "/etc/os-release"
 
-# We log the execution of the script into log.txt
-> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-$ESUDO chmod +x -R $GAMEDIR/*
+GAMEDIR=/$directory/ports/thestarlitescape
+exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-#Exports
-export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$GAMEDIR/utils/libs:$LD_LIBRARY_PATH"
+if [ "$OS_NAME" == "JELOS" ]; then
+  export SPA_PLUGIN_DIR="/usr/lib32/spa-0.2"
+  export PIPEWIRE_MODULE_DIR="/usr/lib32/pipewire-0.3/"
+fi
+
+export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$GAMEDIR/utils/libs"
 export GMLOADER_DEPTH_DISABLE=1
 export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
-export GMLOADER_PLATFORM="os_windows"
 
 cd $GAMEDIR
+
+[ -f "./gamedata/data.win" ] && mv gamedata/data.win gamedata/game.droid
+[ -f "./gamedata/game.win" ] && mv gamedata/game.win gamedata/game.droid
 
 $ESUDO chmod 666 /dev/uinput
 
 $GPTOKEYB "gmloader" -c ./thestarlitescape.gptk &
 
-pm_platform_helper "$GAMEDIR/gmloader"
+$ESUDO chmod +x "$GAMEDIR/gmloader"
+
 ./gmloader thestarlitescape.apk
 
-pm_finish
-
+$ESUDO kill -9 $(pidof gptokeyb)
+$ESUDO systemctl restart oga_events &
+printf "\033c" > /dev/tty0
