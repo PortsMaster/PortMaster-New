0cd55a8d0840761ab102d54e3cae3b80d9906bdc
tabreturn <github@tabreturn.com>
2024-11-15 08:49:34 +1100

    - update port files

--- ports/gloomreducer/Gloom Reducer.sh
+++ ports/gloomreducer/Gloom Reducer.sh
@@ -13,47 +13,30 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
 export PORT_32BIT="Y"
-
-get_controls
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
-
-$ESUDO chmod 666 /dev/tty0
+get_controls
 
 GAMEDIR="/$directory/ports/gloomreducer"
 
-export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs"
+export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$LD_LIBRARY_PATH"
 export GMLOADER_DEPTH_DISABLE=1
 export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
 export GMLOADER_PLATFORM="os_linux"
 
-# We log the execution of the script into log.txt
-exec > >(tee "$GAMEDIR/log.txt") 2>&1
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
 cd $GAMEDIR
 
-if [ -f "${controlfolder}/libgl_${CFWNAME}.txt" ]; then 
-  source "${controlfolder}/libgl_${CFW_NAME}.txt"
-else
-  source "${controlfolder}/libgl_default.txt"
-fi
-
-# Check for file existence before trying to manipulate them:
 [ -f "./gamedata/data.win" ] && mv gamedata/data.win gamedata/game.droid
 [ -f "./gamedata/game.win" ] && mv gamedata/game.win gamedata/game.droid
 [ -f "./gamedata/game.unx" ] && mv gamedata/game.unx gamedata/game.droid
-
-# Make sure uinput is accessible so we can make use of the gptokeyb controls
-$ESUDO chmod 666 /dev/uinput
+rm -f gamedata/*.{dll,exe}
 
 $GPTOKEYB "gmloader" -c ./gloomreducer.gptk &
 
 $ESUDO chmod +x "$GAMEDIR/gmloader"
-
+pm_platform_helper "$GAMEDIR/gmloader"
 ./gmloader gloomreducer.apk
 
-$ESUDO kill -9 $(pidof gptokeyb)
-$ESUDO systemctl restart oga_events &
-printf "\033c" > /dev/tty0
-
+pm_finish
--- ports/gloomreducer/README.md
+++ ports/gloomreducer/README.md
@@ -1,6 +1,6 @@
 ## Notes
 
-Thanks to [owch (chloe)](https://owch.itch.io) for creating this game and making it available for free download on [itch](https://owch.itch.io/gloom-reducer)
+Thanks to [owch (Chloe)](https://owch.itch.io) for creating this game, which you can purchase on [itch](https://owch.itch.io/gloom-reducer)
 
 
 ## Controls
@@ -11,3 +11,4 @@ Thanks to [owch (chloe)](https://owch.itch.io) for creating this game and making
 | X      | Item                 |
 | A      | Swap                 |
 | Start  | Menu                 |
+| R1     | Back                 |
