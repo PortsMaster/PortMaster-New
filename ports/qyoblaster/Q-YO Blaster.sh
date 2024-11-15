3ef58423e1057fc0ffb116393a28f03214c04734
tabreturn <github@tabreturn.com>
2024-11-16 08:43:13 +1100

    - swap buttons

--- ports/qyoblaster/Q-YO Blaster.sh
+++ ports/qyoblaster/Q-YO Blaster.sh
@@ -55,7 +55,8 @@ if [ -f "$GAMEDIR/gamedata/game.droid" ]; then
 fi
 
 # Swap buttons
-$ESUDO chmod +x "$GAMEDIR/tools/SDL_swap_gpbuttons.py"
+"$GAMEDIR/tools/SDL_swap_gpbuttons.py" -i "$SDL_GAMECONTROLLERCONFIG_FILE" -o "$GAMEDIR/gamecontrollerdb_swapped.txt" -l "$GAMEDIR/SDL_swap_gpbuttons.txt"
+export SDL_GAMECONTROLLERCONFIG_FILE="$GAMEDIR/gamecontrollerdb_swapped.txt"
 export SDL_GAMECONTROLLERCONFIG="`echo "$SDL_GAMECONTROLLERCONFIG" | "$GAMEDIR/tools/SDL_swap_gpbuttons.py" -l "$GAMEDIR/SDL_swap_gpbuttons.txt"`"
 
 # Run the game
