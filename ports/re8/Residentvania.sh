90e61d10325649fc40be059e7c5aae0cd938ab9c
Damian2809 <lothk113@gmail.com>
2024-11-27 14:18:59 +0100

    Update Residentvania.sh

--- ports/re8/Residentvania.sh
+++ ports/re8/Residentvania.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
 if [ -d "/opt/system/Tools/PortMaster/" ]; then
   controlfolder="/opt/system/Tools/PortMaster"
 elif [ -d "/opt/tools/PortMaster/" ]; then
@@ -11,10 +13,13 @@ else
 fi
 
 source $controlfolder/control.txt
+
 export PORT_32BIT="Y"
 
-[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 get_controls
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+
+$ESUDO chmod 666 /dev/tty0
 
 GAMEDIR="/$directory/ports/re8"
 
@@ -24,18 +29,28 @@ export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
 export GMLOADER_PLATFORM="os_linux"
 
 # We log the execution of the script into log.txt
-> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
 cd $GAMEDIR
+
+# patch if it hasn't been patched yet
+if [ -f "$GAMEDIR/gamedata/data.win" ]; then
+  $controlfolder/xdelta3 -d -s "$GAMEDIR/gamedata/data.win" "re8.xdelta" "$GAMEDIR/gamedata/game.droid"
+  rm -f "$GAMEDIR/gamedata/data.win"
+fi
+
 
 # Check for file existence before trying to manipulate them:
 [ -f "./gamedata/data.win" ] && mv gamedata/data.win gamedata/game.droid
 [ -f "./gamedata/game.win" ] && mv gamedata/game.win gamedata/game.droid
 [ -f "./gamedata/game.unx" ] && mv gamedata/game.unx gamedata/game.droid
 
+# Make sure uinput is accessible so we can make use of the gptokeyb controls
+$ESUDO chmod 666 /dev/uinput
+
 $GPTOKEYB "gmloader" -xbox360 &
 
-pm_platform_helper "$GAMEDIR/gmloader"
+$ESUDO chmod +x "$GAMEDIR/gmloader"
 
 ./gmloader re8.apk
 
