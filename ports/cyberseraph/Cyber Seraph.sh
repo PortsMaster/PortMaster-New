5e63122bfdb2e380cc1955a81957796c7b60f6c5
tabreturn <github@tabreturn.com>
2024-11-29 20:46:00 +1100

    - add port files

---
+++ ports/cyberseraph/Cyber Seraph.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR="/$directory/ports/cyberseraph"
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x -R $GAMEDIR/*
+
+# Exports
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+# Pack oggs into apk
+if [ -n "$(ls ./gamedata/*.ogg 2>/dev/null)" ]; then
+    mkdir -p ./assets
+    mv ./gamedata/*.ogg ./assets/ 2>/dev/null
+    pm_message "Moved .ogg files from ./gamedata to ./assets/"
+    zip -r -0 ./voidbreach.port ./assets/
+    pm_message "Zipped contents to ./voidbreach.port"
+    rm -Rf ./assets/
+fi
+
+# Prepare files
+[ -f "./gamedata/data.win" ] && mv gamedata/data.win gamedata/game.droid
+[ -f "./gamedata/*.exe" ] && rm -f ./gamedata/*.exe
+
+# Display loading splash
+$ESUDO ./tools/splash "splash.png" 2000
+
+# Assign configs and load the game
+$GPTOKEYB "gmloader.aarch64" -c "cyberseraph.gptk" &
+pm_platform_helper "gmloader.aarch64"
+./gmloader.aarch64 -c gmloader.json
+
+# Cleanup
+pm_finish
