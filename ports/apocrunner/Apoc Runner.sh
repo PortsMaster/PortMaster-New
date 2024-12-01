fc24e42eb37e70427e6b5b88e777aa0c940668e6
t0b10 <t0b10@null.void>
2024-11-30 17:23:19 -0700

    Added missing dependency. Fixed port structure.

--- ports/apocrunner/Apoc Runner.sh
+++ ports/apocrunner/Apoc Runner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# PORTMASTER: apocrunner.zip, Apoc Runner.sh
 
 XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 
@@ -21,22 +22,19 @@ GAMEDIR="/$directory/ports/apocrunner"
 SPLASHFILE="splash.png"
 
 # CD and set permissions
-cd "$GAMEDIR"
-> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+cd "$GAMEDIR" > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 $ESUDO chmod +x "$GAMEDIR/gmloader.aarch64"
 $ESUDO chmod +x "$GAMEDIR/tools/splash"
 
 # Exports
-export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$GAMEDIR/lib:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
-# Display loading splash
-# Note: Due to bug in muOS, we must call twice to ensure it is displayed
+# Display loading splash -  note: Due to bug in muOS, we call twice else not displayed
 if [ "$CFW_NAME" == "muOS" ]; then
   $ESUDO ./tools/splash $SPLASHFILE 1 
 fi
 $ESUDO ./tools/splash $SPLASHFILE 5000
-
 
 # Assign configs and load the game
 $GPTOKEYB "gmloader.aarch64" &
