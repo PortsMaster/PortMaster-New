2f4489efb69a9bbb174c0abbc999818eca76c924
Ganimoth <143072466+Ganimoth@users.noreply.github.com>
2024-12-04 13:25:40 +0100

    Update ports/plunderislands/Plunder Islands.sh
    Co-authored-by: Jacob <kloptops@gmail.com>

--- ports/plunderislands/Plunder Islands.sh
+++ ports/plunderislands/Plunder Islands.sh
@@ -57,7 +57,7 @@ PATH="$godot_dir:$PATH"
 export FRT_NO_EXIT_SHORTCUTS=FRT_NO_EXIT_SHORTCUTS
 
 $GPTOKEYB "$runtime" -c "plunderislands.gptk" &
-pm_platform_helper "$runtime"
+pm_platform_helper "$godot_dir/$runtime"
 "$runtime" $GODOT_OPTS --main-pack "gamedata/plunderislandspatched.pck"
 
 
