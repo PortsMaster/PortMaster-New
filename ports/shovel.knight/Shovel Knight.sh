6854e17576ccc3c4c016061935c74477f7b5ecd4
t0b10 <t0b10@null.void>
2024-12-13 05:16:36 -0700

    Tiny change to resstore Shovel Knight.

--- ports/shovel.knight/Shovel Knight.sh
+++ ports/shovel.knight/Shovel Knight.sh
@@ -13,7 +13,6 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
 export PORT_32BIT="Y"
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 get_controls
