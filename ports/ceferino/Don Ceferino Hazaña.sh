abb519940c6a635966f6fc90e5670d39a46330aa
t0b10 <t0b10@null.void>
2024-12-18 14:27:31 -0700

    Switch over port to bind_directories, remove no longer needed .gitkeep file, and general tidy up.

--- ports/ceferino/Don Ceferino Hazaña.sh
+++ ports/ceferino/Don Ceferino Hazaña.sh
@@ -13,6 +13,7 @@ else
 fi
 
 source $controlfolder/control.txt
+
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 
 get_controls
@@ -23,19 +24,14 @@ BINARY=ceferino
 
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-mkdir -p "$GAMEDIR/conf"
+cd $GAMEDIR
+
+bind_files ~/.ceferino $GAMEDIR/conf/.ceferino
 
 export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 export XDG_DATA_HOME="$CONFDIR"
 export TEXTINPUTINTERACTIVE="Y" 
-
-# Must wait until bind_files comes out of beta
-$ESUDO rm -rf ~/.ceferino
-ln -sfv $GAMEDIR/conf/.ceferino ~/
-# bind_files ~/.ceferino $GAMEDIR/conf/.ceferino
-
-cd $GAMEDIR
 
 $GPTOKEYB "$BINARY" -c ./$BINARY.gptk &
 pm_platform_helper "$GAMEDIR/$BINARY"
