52038da103e0b5cc5952e3e5785a9c752b6f2c80
Fraxinus88 <157858080+Fraxinus88@users.noreply.github.com>
2024-12-29 22:48:11 +0800

    Change to gmloader

--- ports/discroom/Disc Room.sh
+++ ports/discroom/Disc Room.sh
@@ -13,55 +13,53 @@ else
 fi
 
 source $controlfolder/control.txt
-[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
-get_controls
+export PORT_32BIT="Y"
 
-# Variables
+get_controls
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+
 GAMEDIR="/$directory/ports/discroom"
 
-# CD and log
-cd $GAMEDIR
-> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-
-#Permissions
-$ESUDO chmod +x $GAMEDIR/tools/splash
-$ESUDO chmod +x $GAMEDIR/tools/patchscript
-$ESUDO chmod +x $GAMEDIR/gmloadernext.aarch64
-
 # Exports
-export LD_LIBRARY_PATH="$GAMEDIR/lib:$LD_LIBRARY_PATH"
-export PATCHER_FILE="$GAMEDIR/tools/patchscript"
+export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export GMLOADER_DEPTH_DISABLE=0
+export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
+export GMLOADER_PLATFORM="os_windows"
+export TOOLDIR="$GAMEDIR/tools"
+export PATH=$PATH:$TOOLDIR
+export PATCHER_FILE="$GAMEDIR/patch/patchscript"
 export PATCHER_GAME="Disc Room"
 export PATCHER_TIME="3 to 5 minutes"
-export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export PATCHDIR=$GAMEDIR
 
-# dos2unix in case we need it
-dos2unix "$GAMEDIR/tools/gmKtool.py"
-dos2unix "$GAMEDIR/tools/Klib/GMblob.py"
-dos2unix "$GAMEDIR/tools/patchscript"
+# We log the execution of the script into log.txt
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-# Check if patchlog.txt to skip patching
-if [ ! -f patchlog.txt ]; then
+# Permissions
+$ESUDO chmod +x "$GAMEDIR/gmloader"
+$ESUDO chmod +x "$TOOLDIR/splash"
+
+cd "$GAMEDIR"
+
+# Run install if needed
+if [ ! -f "$GAMEDIR/gamedata/game.droid" ]; then
     if [ -f "$controlfolder/utils/patcher.txt" ]; then
         source "$controlfolder/utils/patcher.txt"
         $ESUDO kill -9 $(pidof gptokeyb)
     else
-        pm_message "This port requires the latest version of PortMaster."
+        echo "This port requires the latest version of PortMaster." > $CUR_TTY
     fi
 else
-    pm_message "Patching process already completed. Skipping."
+    echo "Patching process already completed. Skipping."
 fi
 
-# Display loading splash
-if [ -f "$GAMEDIR/patchlog.txt" ]; then
-    $ESUDO ./tools/splash "splash.png" 1 
-    $ESUDO ./tools/splash "splash.png" 2000 &
+if [ ! -f "$GAMEDIR/gamedata/config.ini" ]; then
+  mv "$GAMEDIR/config.ini.default" "$GAMEDIR/gamedata/config.ini"
 fi
 
-# Assign gptokeyb and load the game
-$GPTOKEYB "gmloadernext.aarch64" &
-pm_platform_helper "$GAMEDIR/gmloadernext.aarch64"
-./gmloadernext.aarch64 -c "gmloader.json"
+$GPTOKEYB "gmloader" &
 
-# Cleanup
+pm_platform_helper "$GAMEDIR/gmloader"
+./gmloader game.apk
+
 pm_finish
