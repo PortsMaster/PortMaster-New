4ee0f24489b6035d74f0e9825710da8516c48ffe
MadShmupper <149628836+MadShmupper@users.noreply.github.com>
2025-01-02 20:31:43 -0800

    Update ROCKHEADS.sh
    erased extra $GAMEDIR/lib

--- ports/rockheads/ROCKHEADS.sh
+++ ports/rockheads/ROCKHEADS.sh
@@ -23,7 +23,7 @@ cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 $ESUDO chmod +x $GAMEDIR/gmloadernext.${DEVICE_ARCH}
 
-export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$GAMEDIR/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
 $GPTOKEYB "gmloadernext.aarch64" &
