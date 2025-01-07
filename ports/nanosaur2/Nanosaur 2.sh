a1e0b8f5971f5aec93952234c32567e904101dc4
Ben Willmore <ben@willmore.eu>
2025-01-07 18:23:44 +0000

    Nanosaur 2: Add libEGL

--- ports/nanosaur2/Nanosaur 2.sh
+++ ports/nanosaur2/Nanosaur 2.sh
@@ -34,6 +34,7 @@ fi
 
 if [ "$LIBGL_FB" != "" ]; then
 export SDL_VIDEO_GL_DRIVER="$GAMEDIR/gl4es.aarch64/libGL.so.1"
+export SDL_VIDEO_EGL_DRIVER="$GAMEDIR/gl4es.aarch64/libEGL.so.1"
 fi 
 
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
