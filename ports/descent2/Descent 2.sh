fb0b7de65373d29933b89847a634892c83d12463
Jeod <47716344+JeodC@users.noreply.github.com>
2025-01-12 09:02:49 -0500

    Update Descent launchscripts

--- ports/descent2/Descent 2.sh
+++ ports/descent2/Descent 2.sh
@@ -14,7 +14,6 @@ fi
 
 source $controlfolder/control.txt
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
-
 get_controls
 
 # Variables
@@ -24,70 +23,37 @@ GAME="d2x-rebirth"
 ASPECT_X=${ASPECT_X:-4}
 ASPECT_Y=${ASPECT_Y:-3}
 
-# CD and set permissions
 cd $GAMEDIR
-> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-rm -rf "$GAMEDIR/config/gamelog.txt"
-$ESUDO chmod +x -R $GAMEDIR/*
 
-# Set config dir
-bind_directories ~/.$GAME $GAMEDIR/config
+# Add some cheats
+if [ ! -f "./cheats.txt" ]; then
+	pm_message "Error: Cheats file not found. No cheats will be used."
+else
+	CHEATS=$(sed -n -E '/^[^#]*=[[:space:]]*1([^0-9#]|$)/s/(=[[:space:]]*1[^0-9#]*)//p' ./cheats.txt | tr -d '\n')
+fi
 
 # Exports
 export LD_LIBRARY_PATH="$GAMEDIR/libs:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 export SDL_FORCE_SOUNDFONTS=1
 export SDL_SOUNDFONTS="$GAMEDIR/soundfont.sf2"
-
-# Add some cheats
-if [ ! -f "./cheats.txt" ]; then
-	echo "Error: Cheats file not found. No cheats will be used." > $CUR_TTY
-else
-	CHEATS=$(sed -n -E '/^[^#]*=[[:space:]]*1([^0-9#]|$)/s/(=[[:space:]]*1[^0-9#]*)//p' ./cheats.txt | tr -d '\n')
-fi
-
 export TEXTINPUTPRESET=$CHEATS
 
 # Edit .cfg file with updated resolution and aspect ratio
-sed -i "s/^ResolutionX=640/ResolutionX=$DISPLAY_WIDTH/g" $GAMEDIR/config/descent.cfg
-sed -i "s/^ResolutionY=480/ResolutionY=$DISPLAY_HEIGHT/g" $GAMEDIR/config/descent.cfg
-sed -i "s/^AspectX=.*/AspectX=$ASPECT_Y/g" $GAMEDIR/config/descent.cfg
-sed -i "s/^AspectY=.*/AspectY=$ASPECT_X/g" $GAMEDIR/config/descent.cfg
+sed -i "s/^ResolutionX=[0-9]\{1,4\}/ResolutionX=$DISPLAY_WIDTH/g" "$GAMEDIR/config/descent.cfg"
+sed -i "s/^ResolutionY=[0-9]\{1,4\}/ResolutionY=$DISPLAY_HEIGHT/g" "$GAMEDIR/config/descent.cfg"
+sed -i "s/^AspectX=[0-9]\{1,2\}/AspectX=$ASPECT_Y/g" "$GAMEDIR/config/descent.cfg"
+sed -i "s/^AspectY=[0-9]\{1,2\}/AspectY=$ASPECT_X/g" "$GAMEDIR/config/descent.cfg"
 
-# List of compatibility firmwares
-CFW_NAMES="ArkOS:ArkOS wuMMLe:ArkOS AeUX:knulli:TrimUI"
-
-# Check if the current CFW name is in the list
-contains() {
-    local value="$CFW_NAME"
-    local item
-    local tmp=$IFS
-    IFS=":" # Use : as the delimiter
-    echo "Checking if CFW_NAME '$value' is in the list..."
-    for item in $CFW_NAMES; do
-        echo "Comparing '$item' with '$value'..."
-        if [ "$item" = "$value" ]; then
-            echo "Match found: '$item'"
-            IFS=$tmp
-            return 0
-        fi
-    done
-    echo "No match found for '$value'."
-    IFS=$tmp
-    return 1
-}
-
-# If it's in the list use the compatibility binary
-if contains; then
-	$GPTOKEYB "$GAME.compat" -c "config/joy.gptk" & 
-    pm_platform_helper "$GAMEDIR/$GAME.compat"
-	./$GAME.compat -hogdir data
-else
-	$GPTOKEYB "$GAME.$DEVICE_ARCH" -c "config/joy.gptk" & 
-    pm_platform_helper "$GAMEDIR/$GAME.$DEVICE_ARCH"
-	./$GAME.$DEVICE_ARCH -hogdir data
+# Use compatibility binary if low glibc
+if [ $CFW_GLIBC -lt 234 ]; then
+	GAME="$GAME.compat"
 fi
+
+# Run game
+$GPTOKEYB "$GAME" -c "config/joy.gptk" &
+pm_platform_helper "$GAMEDIR/$GAME"
+./$GAME -hogdir data
 
 # Cleanup
 pm_finish
-
