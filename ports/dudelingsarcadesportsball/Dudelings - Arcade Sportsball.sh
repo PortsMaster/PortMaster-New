621a8e6a24133bf82d7541189a11e5f1ed6cecc6
moocow1452 <moocow1452@gmail.com>
2025-01-21 15:56:28 -0500

    Update ports/dudelingsarcadesportsball/Dudelings - Arcade Sportsball.sh
    Co-authored-by: Jacob <kloptops@gmail.com>

--- ports/dudelingsarcadesportsball/Dudelings - Arcade Sportsball.sh
+++ ports/dudelingsarcadesportsball/Dudelings - Arcade Sportsball.sh
@@ -67,7 +67,7 @@ PATH="$godot_dir:$PATH"
 export FRT_NO_EXIT_SHORTCUTS=FRT_NO_EXIT_SHORTCUTS
 
 $GPTOKEYB "$runtime" -c "./dudelingsarcadesportsball.gptk" &
-pm_platform_helper "$runtime" 
+pm_platform_helper "$godot_dir/$runtime" 
 "$runtime" $GODOT_OPTS --main-pack "gamedata/dudelings.zip"
 
 $ESUDO umount "$godot_dir"
