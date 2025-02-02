5c3f24ab97950351e3925c64244ff108d0352012
JanTrueno <159560609+JanTrueno@users.noreply.github.com>
2025-02-03 00:12:34 +0100

    Update ports/lugaruhd/Lugaru HD.sh
    Co-authored-by: Cyril Delétré <cyril.deletre@gmail.com>

--- ports/lugaruhd/Lugaru HD.sh
+++ ports/lugaruhd/Lugaru HD.sh
@@ -46,8 +46,8 @@ else
 fi
 
 if [ "$LIBGL_FB" != "" ]; then
-export SDL_VIDEO_GL_DRIVER="$GAMEDIR/gl4es.aarch64/libGL.so.1"
-export SDL_VIDEO_EGL_DRIVER="$GAMEDIR/gl4es.aarch64/libEGL.so.1"
+export SDL_VIDEO_GL_DRIVER="$GAMEDIR/gl4es.${DEVICE_ARCH}/libGL.so.1"
+export SDL_VIDEO_EGL_DRIVER="$GAMEDIR/gl4es.${DEVICE_ARCH}/libEGL.so.1"
 fi
 
 # Run port
