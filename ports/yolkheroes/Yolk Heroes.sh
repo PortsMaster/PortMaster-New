4f27c8e6e22885bea64c7762bfa15a4efc39d615
Jeod <47716344+JeodC@users.noreply.github.com>
2025-02-04 08:57:28 -0500

    Fix Yolk Heroes

--- ports/yolkheroes/Yolk Heroes.sh
+++ ports/yolkheroes/Yolk Heroes.sh
@@ -13,19 +13,26 @@ else
 fi
 
 source $controlfolder/control.txt
-
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
-
 get_controls
 
+# Variables
 GAMEDIR="/$directory/ports/yolkheroes/"
 CONFDIR="$GAMEDIR/conf/"
+
+# CD and set permissions
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+$ESUDO chmod +x -R $GAMEDIR/*
+
 # Ensure the conf directory exists
 mkdir -p "$GAMEDIR/conf"
 
 # Set the XDG environment variables for config & savefiles
 export XDG_CONFIG_HOME="$CONFDIR"
 export XDG_DATA_HOME="$CONFDIR"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+export FRT_NO_EXIT_SHORTCUTS=FRT_NO_EXIT_SHORTCUTS
 
 # Keyboard Entry; edit name here
 export TEXTINPUTPRESET="NAME"         # defines preset text to insert
@@ -33,13 +40,11 @@ export TEXTINPUTINTERACTIVE="Y"        # enables interactive text input mode
 export TEXTINPUTNOAUTOCAPITALS="Y"     # disables automatic capitalisation of first letter of words in interactive text input mode
 export TEXTINPUTADDEXTRASYMBOLS="Y"    # enables additional symbols for interactive text input
 
-cd $GAMEDIR
-
-runtime="frt_3.5.2"
+runtime="frt_3.6"
 if [ ! -f "$controlfolder/libs/${runtime}.squashfs" ]; then
   # Check for runtime if not downloaded via PM
   if [ ! -f "$controlfolder/harbourmaster" ]; then
-    echo "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info." > /dev/tty0
+    pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info."
     sleep 5
     exit 1
   fi
@@ -55,14 +60,9 @@ $ESUDO umount "$godot_file" || true
 $ESUDO mount "$godot_file" "$godot_dir"
 PATH="$godot_dir:$PATH"
 
-export FRT_NO_EXIT_SHORTCUTS=FRT_NO_EXIT_SHORTCUTS
-
 $GPTOKEYB "$runtime" -c "./yolkheroes.gptk" &
-pm_platform_helper "$runtime"
-SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig" "$runtime" $GODOT_OPTS --main-pack "gamedata/yolkheroes.pck"
+pm_platform_helper "$runtime" >/dev/null
+"$runtime" $GODOT_OPTS --main-pack "gamedata/yolkheroes.pck"
 
-if [[ "$PM_CAN_MOUNT" != "N" ]]; then
-  $ESUDO umount "$godot_dir"
-fi
-
+# Cleanup
 pm_finish
