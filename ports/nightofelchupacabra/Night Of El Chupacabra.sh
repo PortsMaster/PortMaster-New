7c3ab88dd3eb62db43d81bc007e4251dd7a85fb3
JanTrueno <159560609+JanTrueno@users.noreply.github.com>
2025-02-14 21:53:51 +0100

    Update ports/nightofelchupacabra/Night Of El Chupacabra.sh
    Co-authored-by: Cyril Delétré <cyril.deletre@gmail.com>

--- ports/nightofelchupacabra/Night Of El Chupacabra.sh
+++ ports/nightofelchupacabra/Night Of El Chupacabra.sh
@@ -34,15 +34,9 @@ export PATH="$TOOLDIR:$PATH"
 
 swapabxy() {
     # Update SDL_GAMECONTROLLERCONFIG to swap a/b and x/y button
-
-    if [ "$CFW_NAME" == "knulli" ] && [ -f "$SDL_GAMECONTROLLERCONFIG_FILE" ];then
-	    # Knulli seems to use SDL_GAMECONTROLLERCONFIG_FILE (on rg40xxh at least)
-        cat "$SDL_GAMECONTROLLERCONFIG_FILE" | swapabxy.py > "$GAMEDIR/gamecontrollerdb_swapped.txt"
-	    export SDL_GAMECONTROLLERCONFIG_FILE="$GAMEDIR/gamecontrollerdb_swapped.txt"
-    else
-        # Other CFW use SDL_GAMECONTROLLERCONFIG
-        export SDL_GAMECONTROLLERCONFIG="`echo "$SDL_GAMECONTROLLERCONFIG" | swapabxy.py`"
-    fi
+    cat "$SDL_GAMECONTROLLERCONFIG_FILE" | swapabxy.py > "$GAMEDIR/gamecontrollerdb_swapped.txt"
+    export SDL_GAMECONTROLLERCONFIG_FILE="$GAMEDIR/gamecontrollerdb_swapped.txt"
+    export SDL_GAMECONTROLLERCONFIG="`echo "$SDL_GAMECONTROLLERCONFIG" | swapabxy.py`"
 }
 
 # Swap a/b and x/y button if needed
