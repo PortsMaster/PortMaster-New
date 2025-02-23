5c8557b2594b6230bd2353e6859b5cb650450eeb
Ben Willmore <ben@willmore.eu>
2025-02-23 21:51:26 +0000

    Fix graphics on rocknix, bundle freeware files, update metadata

--- ports/doukutsu-rs/Doukutsu-rs.sh
+++ ports/doukutsu-rs/Doukutsu-rs.sh
@@ -28,6 +28,15 @@ cd $GAMEDIR
 
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
+if [[ -e $GAMEDIR/data ]]; then
+  echo "Game files found"
+else
+  echo "Game files not found, using Aeon Genesis translation"
+  unzip cavestoryen.zip
+  mv CaveStory/* .
+  rmdir CaveStory
+fi
+
 # remove extraneous files
 rm -rf Config.dat DoConfig.exe OrgView.exe *.dll
 
@@ -35,15 +44,19 @@ bind_directories ~/.local/share/doukutsu-rs $GAMEDIR/conf
 
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
-if [ -f "${controlfolder}/libgl_${CFW_NAME}.txt" ]; then 
-  source "${controlfolder}/libgl_${CFW_NAME}.txt"
-else
-  source "${controlfolder}/libgl_default.txt"
-fi
+if [[ ${CFW_NAME} != ROCKNIX ]]; then
+  # GL4ES is not needed on rocknix
 
-if [ "$LIBGL_FB" != "" ]; then
-export SDL_VIDEO_GL_DRIVER="$GAMEDIR/gl4es.aarch64/libGL.so.1"
-export SDL_VIDEO_EGL_DRIVER="$GAMEDIR/gl4es.aarch64/libEGL.so.1"
+  if [ -f "${controlfolder}/libgl_${CFW_NAME}.txt" ]; then
+    source "${controlfolder}/libgl_${CFW_NAME}.txt"
+  else
+    source "${controlfolder}/libgl_default.txt"
+  fi
+
+  if [ "$LIBGL_FB" != "" ]; then
+    export SDL_VIDEO_GL_DRIVER="$GAMEDIR/gl4es.aarch64/libGL.so.1"
+    export SDL_VIDEO_EGL_DRIVER="$GAMEDIR/gl4es.aarch64/libEGL.so.1"
+  fi
 fi
 
 $GPTOKEYB "$BINARY" -c "./$BINARY.gptk" &
