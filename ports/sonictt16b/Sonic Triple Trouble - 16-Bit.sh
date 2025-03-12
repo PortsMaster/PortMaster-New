c16180fd64ccacc0ed838f205d0ea50ace49b942
Jeod <47716344+JeodC@users.noreply.github.com>
2025-03-12 10:42:46 -0400

    Refactor

--- ports/sonictt16b/Sonic Triple Trouble - 16-Bit.sh
+++ ports/sonictt16b/Sonic Triple Trouble - 16-Bit.sh
@@ -22,18 +22,12 @@ GAMEDIR="/$directory/ports/sonictt16b"
 # CD and set permissions
 cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
-$ESUDO chmod +x -R $GAMEDIR/*
 
 # Exports
-export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$GAMEDIR/libs:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$LD_LIBRARY_PATH"
 export PATCHER_FILE="$GAMEDIR/tools/patchscript"
 export PATCHER_GAME="$(basename "${0%.*}")" # This gets the current script filename without the extension
 export PATCHER_TIME="10 to 15 minutes"
-
-# dos2unix in case we need it
-dos2unix "$GAMEDIR/tools/gmKtool.py"
-dos2unix "$GAMEDIR/tools/Klib/GMblob.py"
-dos2unix "$GAMEDIR/tools/patchscript"
 
 # Check if patchlog.txt to skip patching
 if [ ! -f patchlog.txt ]; then
@@ -49,14 +43,14 @@ fi
 
 # Display loading splash
 if [ -f "$GAMEDIR/patchlog.txt" ]; then
-[ "$CFW_NAME" == "muOS" ] && $ESUDO ./libs/splash "splash.png" 1 
-    $ESUDO ./libs/splash "splash.png" 2000
+    $ESUDO ./tools/splash "splash.png" 1 
+    $ESUDO ./tools/splash "splash.png" 5000 &
 fi
 
 # Run the game
-$GPTOKEYB "gmloadernext" -c "./sonic.gptk" &
-pm_platform_helper "$GAMEDIR/gmloadernext"
-./gmloadernext
+$GPTOKEYB "gmloadernext.aarch64" -c "./sonic.gptk" &
+pm_platform_helper "$GAMEDIR/gmloadernext.aarch64" >/dev/null
+./gmloadernext.aarch64 -c gmloader.json
 
 # Kill processes
 pm_finish
