036bd85b0a5b548a1ffa01d2233d9b1088b205a5
Ophir Doron <tulidoron@gmail.com>
2025-04-14 14:55:15 +0300

    Update Pip Flip Paradise.sh

--- ports/pipflipparadise/Pip Flip Paradise.sh
+++ ports/pipflipparadise/Pip Flip Paradise.sh
@@ -25,7 +25,6 @@ cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 $ESUDO chmod +x -R $GAMEDIR/gmloadernext.aarch64
 $ESUDO chmod +x -R $GAMEDIR/tools/splash
-$ESUDO chmod +x -R $GAMEDIR/tools/gmKtool.py
 
 # Exports
 export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$GAMEDIR/libs:$LD_LIBRARY_PATH"
