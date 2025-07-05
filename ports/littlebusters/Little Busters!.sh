1a65ce9e97fb704844b4c619b66d47f13028f143
Jacob <kloptops@gmail.com>
2025-07-05 14:46:12 +0800

    Updated rlvm bash scripts to use bind_directory

--- ports/littlebusters/Little Busters!.sh
+++ ports/littlebusters/Little Busters!.sh
@@ -52,8 +52,7 @@ $ESUDO mount "$rlvm_file" "$rlvm_dir"
 PATH="$rlvm_dir:$PATH"
 
 # Create config dir
-rm -rf "$HOME/.rlvm/KEY_リトルバスターズ！"
-ln -s "$GAMEDIR/saves" "$HOME/.rlvm/KEY_リトルバスターズ！"
+bind_directories "$HOME/.rlvm/KEY_リトルバスターズ！" "$GAMEDIR/saves/"
 
 # Export libs
 export LD_LIBRARY_PATH="$rlvm_dir/libs":$LD_LIBRARY_PATH
