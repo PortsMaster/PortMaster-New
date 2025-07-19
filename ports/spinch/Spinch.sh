f5217b9fc9c66733b84f0548160b40ab052e7913
Fraxinus88 <157858080+Fraxinus88@users.noreply.github.com>
2025-07-19 18:07:22 +0800

    Update ports/spinch/Spinch.sh
    Co-authored-by: Cyril Delétré <cyril.deletre@gmail.com>

--- ports/spinch/Spinch.sh
+++ ports/spinch/Spinch.sh
@@ -12,7 +12,6 @@ else
   controlfolder="/roms/ports/PortMaster"
 fi
 
-export controlfolder 
 
 source $controlfolder/control.txt
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
