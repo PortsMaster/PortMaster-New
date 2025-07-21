466cf7f244aacee52179f6d28f1d22c0078834c8
RoyalJupiter <ztoth@users.noreply.github.com>
2025-07-21 10:59:29 -0700

    remove zip

--- ports/nazizombies/Nazi Zombies.sh
+++ ports/nazizombies/Nazi Zombies.sh
@@ -27,38 +27,56 @@ cd "$GAMEDIR"
 
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-# Check if the game is already installed, run the patch script otherwise
+echo "Device architecture is $DEVICE_ARCH"
+echo "Glibc version is $CFW_GLIBC"
+echo "Screen resolution $DISPLAY_WIDTH x $DISPLAY_HEIGHT"
+
+# Check if the game is already configured
 if [ ! -f "$RUNDIR/$BINARY" ]; then
-  export PATCHER_FILE="$TOOLDIR/patchscript"
-  export PATCHER_TIME="2 minutes"
+  echo "First time run"
+  cd "$RUNDIR"
 
-  if [ -f "$controlfolder/utils/patcher.txt" ]; then
-    $ESUDO chmod a+x "$TOOLDIR/patchscript"
-    source "$controlfolder/utils/patcher.txt"
-    $ESUDO kill -9 $(pidof gptokeyb)
+  if [ "$CFW_GLIBC" -lt 231 ]; then
+    cp "nzp-glibc230-sdl" $BINARY
   else
-    pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info."
+    cp "nzp-$DEVICE_ARCH-sdl" $BINARY
   fi
 
-  if [ ! -f "$RUNDIR/$BINARY" ]; then
-    pm_message "Installation failed, please check the logs and the README for details."
-  fi
+  chmod 777 $BINARY
+
+  # patch config
+  echo "Patching config"
+  CONFIGDIR="$RUNDIR"/nzp
+  CONFIGFILE="$CONFIGDIR"/nzportable.cfg
+  cd "$CONFIGDIR"
+  sed -i -E "s/vid_width.*/vid_width \"$DISPLAY_WIDTH\"/" $CONFIGFILE
+  sed -i -E "s/vid_height.*/vid_height \"$DISPLAY_HEIGHT\"/" $CONFIGFILE
+  sed -i -E "s/vid_conwidth.*/vid_conwidth \"$DISPLAY_WIDTH\"/" $CONFIGFILE
+  sed -i -E "s/vid_conheight.*/vid_conheight \"$DISPLAY_HEIGHT\"/" $CONFIGFILE
+  sed -i -E "s/sensitivity.*/sensitivity \"1\"/" $CONFIGFILE
+  sed -i -E "s/sensitivity.*/sensitivity \"1\"/" $CONFIGFILE
+  sed -i -E "s/joyyawsensitivity.*/joyyawsensitivity \"0.3\"/" $CONFIGFILE
+  sed -i -E "s/joypitchsensitivity.*/joypitchsensitivity \"0.05\"/" $CONFIGFILE
+  sed -i -E "s/bind.*GP_LSHOULDER.*/bindlevel GP_LSHOULDER 30 \"+button8\"/" $CONFIGFILE
+  sed -i -E "s/bind.*GP_RSHOULDER.*/bindlevel GP_RSHOULDER 30 \"+attack\"/" $CONFIGFILE
+  sed -i -E "s/bind.*GP_LTRIGGER.*/bindlevel GP_LTRIGGER 30 \"impulse 33\"/" $CONFIGFILE
+  sed -i -E "s/bind.*GP_RTRIGGER.*/bindlevel GP_RTRIGGER 30 \"+button3\"/" $CONFIGFILE
+  sed -i -E "s/name \"Unknown Soldier\"/name \"PortMaster Soldier\"/" $CONFIGFILE
 else
-  pm_message "Game files found. Skipping."
+  # Update screen resolution settings
+  if [ -f "$RUNDIR/nzp/user_settings.cfg" ]; then
+    CONFIGFILE="$RUNDIR"/nzp/user_settings.cfg
+  else
+    CONFIGFILE="$RUNDIR"/nzp/nzportable.cfg
+  fi
+  echo "Updating resolution in $CONFIGFILE"
+  sed -i -E "s/vid_width.*/vid_width \"$DISPLAY_WIDTH\"/" $CONFIGFILE
+  sed -i -E "s/vid_height.*/vid_height \"$DISPLAY_HEIGHT\"/" $CONFIGFILE
+  sed -i -E "s/vid_conwidth.*/vid_conwidth \"$DISPLAY_WIDTH\"/" $CONFIGFILE
+  sed -i -E "s/vid_conheight.*/vid_conheight \"$DISPLAY_HEIGHT\"/" $CONFIGFILE
 fi
 
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-
-# Update game settings file with screen resolution
-if [ -f "$RUNDIR/nzp/user_settings.cfg" ]; then
-  CONFIGFILE="$RUNDIR"/nzp/user_settings.cfg
-else
-  CONFIGFILE="$RUNDIR"/nzp/nzportable.cfg
-fi
-sed -i -E "s/vid_width.*/vid_width \"$DISPLAY_WIDTH\"/" $CONFIGFILE
-sed -i -E "s/vid_height.*/vid_height \"$DISPLAY_HEIGHT\"/" $CONFIGFILE
-sed -i -E "s/vid_conwidth.*/vid_conwidth \"$DISPLAY_WIDTH\"/" $CONFIGFILE
-sed -i -E "s/vid_conheight.*/vid_conheight \"$DISPLAY_HEIGHT\"/" $CONFIGFILE
 
 cd "$RUNDIR"
 
---
+++ ports/nazizombies/nazizombies/game/nzp/gfx/env/READMEWaldon.txt
@@ -0,0 +1,17 @@
+https://gamebanana.com/mods/6814
+Made by JasperCarmack
+
+02/02/2020 
+==============================================
+Sky_Waldon01 VMT, VTF and TGAs
+==============================================
+
+ABOUT THIS FILE:
+
+A gorgeous afternoon sky.
+
+Feel free to use for your own projects and mods! Credit not required but is always appreciated.
+
+Commercial use restricted; rendered on an unlicenced copy of Terragen Classic, so if you go using this for a retail game, it ain't me you have to worry about, Planetside would probably want to have a word with you.
+
+ENJOY!
