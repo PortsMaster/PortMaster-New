22e9fca3feba8acb431c333651f8296645e5cbf4
Fraxinus88 <157858080+Fraxinus88@users.noreply.github.com>
2025-08-25 11:34:32 +0800

    Update to gmloader.next and ready to run!

--- ports/youhave10s2023/You Have 10 Seconds (2023).sh
+++ ports/youhave10s2023/You Have 10 Seconds (2023).sh
@@ -13,63 +13,38 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
-export PORT_32BIT="Y"
-
-get_controls
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
 
-$ESUDO chmod 666 /dev/tty0
-
+# Variables
 GAMEDIR="/$directory/ports/youhave10s2023"
+GMLOADER_JSON="$GAMEDIR/gmloader.json"
+TOOLDIR="$GAMEDIR/tools"
 
-export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$GAMEDIR/utils/libs:$LD_LIBRARY_PATH"
-export GMLOADER_DEPTH_DISABLE=1
-export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
-
-# We log the execution of the script into log.txt
-exec > >(tee "$GAMEDIR/log.txt") 2>&1
-
+# CD and set permissions
 cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-if [ -f "${controlfolder}/libgl${CFWNAME}.txt" ]; then 
-  source "${controlfolder}/libgl${CFW_NAME}.txt"
-else
-  source "${controlfolder}/libgl_default.txt"
+# Exports
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+# Ensure executable permissions
+$ESUDO chmod +x -R $GAMEDIR/gmloadernext.aarch64
+$ESUDO chmod +x -R $GAMEDIR/tools/splash
+
+# Make saves dir if not present
+if [ ! -d "saves" ]; then
+    mkdir -p saves 
 fi
 
-# Check for You Have 10 Seconds 2023.exe and delete if it exists
-if [ -f "$GAMEDIR/gamedata/You Have 10 Seconds 2023.exe" ]; then
-    rm "$GAMEDIR/gamedata/You Have 10 Seconds 2023.exe"
-    echo "Deleted You Have 10 Seconds 2023.exe from gamedata folder."
-fi
+# Display loading splash
+$ESUDO "$GAMEDIR/tools/splash" "$GAMEDIR/splash.png" 4000 & 
 
-# Check if there are .ogg files in ./gamedata
-if [ -n "$(ls ./gamedata/*.ogg 2>/dev/null)" ]; then
-    # Move all .ogg files from ./gamedata to ./assets
-    mv ./gamedata/*.ogg ./assets/
-    echo "Moved .ogg files from ./gamedata to ./assets/"
+# Assign configs and load the game
+$GPTOKEYB "gmloadernext.aarch64" -c "youhave10s2023.gptk" &
+pm_platform_helper "$GAMEDIR/gmloadernext.aarch64"
+./gmloadernext.aarch64 -c "$GMLOADER_JSON"
 
-    # Zip the contents of ./sm.apk including the new .ogg files
-    zip -r ./youhave10s2023.apk ./youhave10s2023.apk ./assets/
-    echo "Zipped contents to ./youhave10s2023.apk"
-fi
-
-# Check for file existence before trying to manipulate them:
-[ -f "./gamedata/data.win" ] && mv gamedata/data.win gamedata/game.droid
-[ -f "./gamedata/game.win" ] && mv gamedata/game.win gamedata/game.droid
-[ -f "./gamedata/game.unx" ] && mv gamedata/game.unx gamedata/game.droid
-
-# Make sure uinput is accessible so we can make use of the gptokeyb controls
-$ESUDO chmod 666 /dev/uinput
-
-$GPTOKEYB "gmloader" -c ./youhave10s2023.gptk &
-
-$ESUDO chmod +x "$GAMEDIR/gmloader"
-
-./gmloader youhave10s2023.apk
-
-$ESUDO kill -9 $(pidof gptokeyb)
-$ESUDO systemctl restart oga_events &
-printf "\033c" > /dev/tty0
-
+# Cleanup
+pm_finish
