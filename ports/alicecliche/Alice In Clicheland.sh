a55c0243206beb3cb1632173c7dfbae6aad85f14
user <25371653+ImCoKeMaN@users.noreply.github.com>
2025-08-26 22:03:30 -0400

    updated sh with proper gptk name

--- ports/alicecliche/Alice In Clicheland.sh
+++ ports/alicecliche/Alice In Clicheland.sh
@@ -49,7 +49,7 @@ if [[ "$PM_CAN_MOUNT" != "N" ]]; then
 fi
 $ESUDO mount "$controlfolder/libs/${weston_runtime}.squashfs" "${weston_dir}"
 
-$GPTOKEYB "$BINARY" -c ./$BINARY.gptk &
+$GPTOKEYB "$BINARY" -c ./AliceInClicheland.gptk &
 pm_platform_helper "$GAMEDIR/$BINARY"
 
 # Start Westonpack
