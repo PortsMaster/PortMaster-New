730b8022e5bf58c148ce5f9d57611a30b4db9fde
tabreturn <github@tabreturn.com>
2025-09-12 20:33:55 +1000

    - add port files

---
+++ ports/foreignfrugglers/Foreign Frugglers.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# variables
+GAMEDIR="/$directory/ports/foreignfrugglers"
+GMLOADER_JSON="$GAMEDIR/gmloader.json"
+
+# cd and logging
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# exports
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$GAMEDIR/lib:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+$ESUDO chmod +x $GAMEDIR/gmloadernext.aarch64
+
+# extract file
+if [ -f "./assets/Foreign Frugglers.exe" ]; then
+  # extract its contents in place using 7z
+  pm_message "extracting Foreign Frugglers.exe ..."
+  ./7z x "./assets/Foreign Frugglers.exe" -o"./assets/" -y
+  pm_message "Extraction complete"
+fi
+
+# prepare game files
+if [ -f ./assets/data.win ]; then
+  mv ./assets/data.win ./assets/game.droid 
+  rm -f assets/*.{dll,exe}
+  zip -r -0 ./foreignfrugglers.port ./assets/
+  rm -Rf ./assets/
+fi
+
+# assign configs and load the game
+$GPTOKEYB "gmloadernext.aarch64" -c "vampiregarden.gptk" &
+pm_platform_helper "$GAMEDIR/gmloadernext.aarch64"
+./gmloadernext.aarch64 -c "$GMLOADER_JSON"
+
+# cleanup
+pm_finish
---
+++ ports/foreignfrugglers/README.md
@@ -0,0 +1,12 @@
+## Notes
+
+Thanks to [UltimoGames](https://ultimogames.itch.io/) for creating this great game, which you can purchase at [itch.io](https://ultimogames.itch.io/foreignfrugglers)
+
+
+## Controls
+
+| Button        | Action   |
+| ------------- | -------- |
+| D-PAD/L-STICK | Movement |
+| A             | Jump     |
+| B             | Shoot    |
