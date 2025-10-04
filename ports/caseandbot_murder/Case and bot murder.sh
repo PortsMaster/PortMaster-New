717cdddd5bf0c8ec7c2dd81ae19814262bc6f2da
Pedro Fernandes <pedro.barata.fernandes@gmail.com>
2025-10-04 06:22:30 +0100

    Update ports/caseandbot_murder/Case and bot murder.sh
    Co-authored-by: tabreturn <github@tabreturn.com>

--- ports/caseandbot_murder/Case and bot murder.sh
+++ ports/caseandbot_murder/Case and bot murder.sh
@@ -69,7 +69,7 @@ elif [ -f "./assets/Case&Bot.exe" ]; then
             exit 1
         fi
     else
-        echo "No data.win file found to patch!"
+        pm_message "No data.win file found to patch!"
     fi
 
 	# Delete all redundant files
