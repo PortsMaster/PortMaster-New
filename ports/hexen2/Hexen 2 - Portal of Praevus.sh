886bfded2191a989d4cdd716a9e89485015b9d8d
Slayer366 <andykratz@hotmail.com>
2025-10-05 21:49:59 -0500

    Minor improvement to shell scripts

--- ports/hexen2/Hexen 2 - Portal of Praevus.sh
+++ ports/hexen2/Hexen 2 - Portal of Praevus.sh
@@ -27,9 +27,9 @@ else
 fi
 
 if [ "$LIBGL_FB" != "" ] && [ "${CFW_NAME^^}" != "KNULLI" ]; then
-  export SDL_VIDEO_GL_DRIVER="$GAMEDIR/gl4es.aarch64/libGL.so.1"
-  export SDL_VIDEO_EGL_DRIVER="$GAMEDIR/gl4es.aarch64/libEGL.so.1"
-fi 
+  export SDL_VIDEO_GL_DRIVER="$GAMEDIR/gl4es.${DEVICE_ARCH}/libGL.so.1"
+  export SDL_VIDEO_EGL_DRIVER="$GAMEDIR/gl4es.${DEVICE_ARCH}/libEGL.so.1"
+fi
 
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
