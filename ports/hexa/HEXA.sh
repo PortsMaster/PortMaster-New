db582892f4c46df07b6bd4b245ac1e416d42cb24
DrAvedi <102281216+DrAvedi@users.noreply.github.com>
2025-10-20 11:57:24 -0700

    bind directories fix
    edited .sh to specify portname in `bind_directories`

--- ports/hexa/HEXA.sh
+++ ports/hexa/HEXA.sh
@@ -30,7 +30,7 @@ export XDG_DATA_HOME="$CONFDIR"
 
 #  If XDG Path does not work
 # Use bind_directories to reroute that to a location within the ports folder.
-bind_directories ~/.portfolder $GAMEDIR/conf/love/hexa
+bind_directories ~/hexa $GAMEDIR/conf/love/hexa
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
 source $controlfolder/runtimes/"love_11.5"/love.txt
