a4a38033db7e02a45a2343c78209ccd7bd1a524b
tabreturn <github@tabreturn.com>
2025-10-26 22:12:57 +1100

    - clean up .sh

--- ports/sadgod/SAD GOD.sh
+++ ports/sadgod/SAD GOD.sh
@@ -94,8 +94,6 @@ fi
 
 $GPTOKEYB "$godot_executable" -c "$GAMEDIR/$gptk_filename" &
 # start westonpack and godot
-# put CRUSTY_SHOW_CURSOR=1 after "env" if you need a mouse cursor
-# LD_PRELOAD is put here because godot runtime links against libegl.so, and crusty is interfering with that on some systems
 $ESUDO env $weston_dir/westonwrap.sh headless noop kiosk crusty_x11egl \
 XDG_DATA_HOME=$CONFDIR $godot_dir/$godot_executable \
 --resolution ${DISPLAY_WIDTH}x${DISPLAY_HEIGHT} -f \
