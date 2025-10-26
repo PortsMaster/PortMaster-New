49a28b77879259a6b343bf55aed906e18da9b9ce
tabreturn <github@tabreturn.com>
2025-10-26 16:37:47 +1100

    - convert to aarch64 gmloader

--- ports/thecaribbeansail/The Caribbean Sail.sh
+++ ports/thecaribbeansail/The Caribbean Sail.sh
@@ -13,55 +13,45 @@ else
 fi
 
 source $controlfolder/control.txt
-export PORT_32BIT="Y"
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
 get_controls
 
-# Variables
+# variables
 GAMEDIR="/$directory/ports/thecaribbeansail"
+GMLOADER_JSON="$GAMEDIR/gmloader.json"
 
-# CD and set permissions
+# cd and set log
 cd $GAMEDIR
 > "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-$ESUDO chmod +x "$GAMEDIR/gmloadernext.armhf"
-$ESUDO chmod +x "$GAMEDIR/tools/splash"
-$ESUDO chmod +x "$GAMEDIR/tools/text_viewer"
-$ESUDO chmod +x "$GAMEDIR/tools/oggenc"
-$ESUDO chmod +x "$GAMEDIR/tools/oggdec"
-$ESUDO chmod +x "$GAMEDIR/tools/gmKtool.py"
+# ensure executable permissions
+$ESUDO chmod +x "$GAMEDIR/gmloadernext.aarch64"
+$ESUDO chmod +x "$GAMEDIR/tools/patchscript"
 
-# Patcher config
-export PATCHER_FILE="$GAMEDIR/tools/patchscript"
-export PATCHER_GAME="$(basename "${0%.*}")" # This gets the current script filename without the extension
-export PATCHER_TIME="2 to 5 minutes"
-
-# Exports
-export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs.armhf:$LD_LIBRARY_PATH"
+# exports
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$GAMEDIR/lib:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
-export PATH="$PATH:$GAMEDIR/tools"
+$ESUDO chmod +x $GAMEDIR/gmloadernext.aarch64
 
-# Check if patchlog.txt to skip patching
-if [ ! -f patchlog.txt ]; then
-    if [ -f "$controlfolder/utils/patcher.txt" ]; then
-        source "$controlfolder/utils/patcher.txt"
-        $ESUDO kill -9 $(pidof gptokeyb)
-    else
-        echo "This port requires the latest version of PortMaster."
-        text_viewer -e -f 25 -w -t "PortMaster needs to be updated" -m "This port requires the latest version of PortMaster. Please update PortMaster first.\n\nPress SELECT to close this window."
-        exit 0
-    fi
-else
-    echo "Patching process already completed. Skipping."
+# check if we need to patch the game
+if [ ! -f install_completed ]; then
+  # move jpg file(s)
+  mv assets/*.jpg ./
+  if [ -f "$controlfolder/utils/patcher.txt" ]; then
+    export PATCHER_FILE="$GAMEDIR/tools/patchscript"
+    export PATCHER_GAME="The Caribbean Sail"
+    export PATCHER_TIME="2 to 5 minutes"
+    source "$controlfolder/utils/patcher.txt"
+    $ESUDO kill -9 $(pidof gptokeyb)
+  else
+    pm_message "This port requires the latest version of PortMaster."
+  fi
 fi
 
-# Display loading splash
-$ESUDO ./tools/splash "splash.png" 2000 &
+# assign gptokeyb and load the game
+$GPTOKEYB "gmloadernext.aarch64" -c "thecaribbeansail.gptk" &
+pm_platform_helper "$GAMEDIR/gmloadernext.aarch64"
+./gmloadernext.aarch64 -c "$GMLOADER_JSON"
 
-# Assign configs and load the game
-$GPTOKEYB "gmloadernext.armhf" -c "thecaribbeansail.gptk" &
-pm_platform_helper "${GAMEDIR}/gmloadernext.armhf"
-./gmloadernext.armhf -c gmloader.json
-
-# Cleanup
+# cleanup
 pm_finish
