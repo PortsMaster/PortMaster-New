5e68b689ff3fc993d07f2db45353b85bcd1f4a61
Ganimoth <ganimoth@gmail.com>
2025-11-05 09:44:29 +0100

    New port: Maou's Crucible

---
+++ ports/maoucrucible/README.md
@@ -0,0 +1,17 @@
+## Notes
+
+Thanks to Maou's Crucible Team for developing this game. Get it on [Steam](https://store.steampowered.com/app/4087610/Maous_Crucible).
+
+## Controls
+
+| Button | Action |
+|--|--| 
+|DPAD / Left stick|Movement|
+|Right stick|Aim|
+|A|Confirm|
+|B|Cancel|
+|X/R1|Attack|
+|R2|Dash|
+|R3|Autoaim toggle|
+
+
---
+++ ports/maoucrucible/maoucrucible/patcherchoice.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        id = "INTERPOLATION",
+        question = "Would you like to enable image interpolation? It can decrease pixel shimmering and noise at certain low resolutions, but it will introduce some blur.",
+        options = {
+            {"true", "Yes"},
+            {"false", "No"}
+        }
+    }
+}
