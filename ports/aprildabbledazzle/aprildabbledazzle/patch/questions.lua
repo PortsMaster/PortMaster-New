4b68be56adabe559b81ba9ccd07f98a5c27d1696
Doronimmo <186748971+Doronimmo@users.noreply.github.com>
2025-11-07 17:57:42 +0200

    Add files via upload

---
+++ ports/aprildabbledazzle/aprildabbledazzle/patch/questions.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        id = "PATCH",
+        question = "Do you want to patch the game so UP can no longer be used as the A button in gameplay?",
+        options = {
+            {"true", "Yes"},
+            {"false", "No"}
+        }
+    }
+}
