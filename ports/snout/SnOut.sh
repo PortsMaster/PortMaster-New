8b58d9caa6117b7fb484a07c56056d830dbb5dfc
Fraxinus88 <ashley.lims@live.com.sg>
2026-01-22 00:45:57 +0800

    Update Snout to gmloader.next
    Tested on relevant CFW and resolutions

--- ports/snout/SnOut.sh
+++ ports/snout/SnOut.sh
@@ -13,60 +13,29 @@ else
 fi
 
 source $controlfolder/control.txt
-source $controlfolder/device_info.txt
-export PORT_32BIT="Y"
-
-get_controls
 [ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
 
-$ESUDO chmod 666 /dev/tty0
-
+# Variables
 GAMEDIR="/$directory/ports/snout"
+GMLOADER_JSON="$GAMEDIR/gmloader.json"
+TOOLDIR="$GAMEDIR/tools"
 
-export LD_LIBRARY_PATH="/usr/lib32:$GAMEDIR/libs:$GAMEDIR/utils/libs:$LD_LIBRARY_PATH"
-export GMLOADER_DEPTH_DISABLE=1
-export GMLOADER_SAVEDIR="$GAMEDIR/gamedata/"
-
-# We log the execution of the script into log.txt
-exec > >(tee "$GAMEDIR/log.txt") 2>&1
-
+# CD and set permissions
 cd $GAMEDIR
-# Check if there are .ogg files in ./gamedata and no .ogg in /assets move them to the appropriate places
-if [[ -n "$(ls ./gamedata/*.ogg 2>/dev/null)" ]] && [[ ! -f "./assets/*.ogg" ]]; then
-    # Copy all .ogg files from ./gamedata to ./assets if there are no .ogg files in ./assets
-    cp ./gamedata/*.ogg ./assets/
-    echo "Copied ogg files from ./gamedata to ./assets/"
-    
-    # Copy *.ini files to ./assets
-    cp ./gamedata/*.ini ./assets/
-    echo "Copied ini files from ./gamedata to ./assets/"
-    
-    # Copy *.roo files to ./assets
-    cp ./gamedata/*.roo ./assets/
-    echo "Copied roo files from ./gamedata to ./assets/"
-    
-    # Zip the contents of ./carriesorderup.apk including the new .ogg files
-    zip -r ./test.apk ./test.apk ./assets/
-    echo "Zipped contents to ./test.apk" 
-else
-    echo "Ogg files already exist in ./assets. Skipping copy and zipping."
-fi
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
 
-# Check for file existence before trying to manipulate them:
-[ -f "./gamedata/data.win" ] && mv gamedata/data.win gamedata/game.droid
-[ -f "./gamedata/game.win" ] && mv gamedata/game.win gamedata/game.droid
-[ -f "./gamedata/game.unx" ] && mv gamedata/game.unx gamedata/game.droid
+# Exports
+export LD_LIBRARY_PATH="/usr/lib:$GAMEDIR/lib:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
-# Make sure uinput is accessible so we can make use of the gptokeyb controls
-$ESUDO chmod 666 /dev/uinput
+# Ensure executable permissions
+$ESUDO chmod +x "$GAMEDIR/gmloadernext.aarch64"
 
-$GPTOKEYB "gmloader" -c ./snout.gptk &
+# Assign configs and load the game
+$GPTOKEYB "gmloadernext.aarch64" -c "snout.gptk" &
+pm_platform_helper "$GAMEDIR/gmloadernext.aarch64"
+./gmloadernext.aarch64 -c "$GMLOADER_JSON"
 
-$ESUDO chmod +x "$GAMEDIR/gmloader"
-
-./gmloader snout.apk
-
-$ESUDO kill -9 $(pidof gptokeyb)
-$ESUDO systemctl restart oga_events &
-printf "\033c" > /dev/tty0
-
+# Cleanup
+pm_finish
