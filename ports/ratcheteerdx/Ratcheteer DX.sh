192d330913030a3615b70ec25dd07aedf138b44a
ImCoKeMaN <25371653+ImCoKeMaN@users.noreply.github.com>
2026-03-02 20:59:23 -0500

    Simplify Ratcheteer DX script by removing Weston setup
    Removed Weston runtime mounting and related checks.

--- ports/ratcheteerdx/Ratcheteer DX.sh
+++ ports/ratcheteerdx/Ratcheteer DX.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# PORTMASTER: ratcheteerdx.zip, Ratcheteer DX.sh
 
 XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 
@@ -39,33 +40,6 @@ $ESUDO mkdir -p "${LOCALDIR}"
 bind_directories ~/.ratcheteerdx "$CONFDIR"
 $ESUDO chmod a+x "$DATADIR/$BINARY"
 
-# Mount Weston runtime
-weston_dir=/tmp/weston
-$ESUDO mkdir -p "${weston_dir}"
-weston_runtime="weston_pkg_0.2"
-if [ ! -f "$controlfolder/libs/${weston_runtime}.squashfs" ]; then
-  if [ ! -f "$controlfolder/harbourmaster" ]; then
-    pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info."
-    sleep 5
-    exit 1
-  fi
-  $ESUDO $controlfolder/harbourmaster --quiet --no-check runtime_check "${weston_runtime}.squashfs"
-fi
-if [[ "$PM_CAN_MOUNT" != "N" ]]; then
-    $ESUDO umount "${weston_dir}"
-fi
-$ESUDO mount "$controlfolder/libs/${weston_runtime}.squashfs" "${weston_dir}"
-
-
-# rocknix mode on rocknix panfrost/freedreno; libmali not supported
-if [[ "$CFW_NAME" = "ROCKNIX" ]]; then
-  export rocknix_mode=1
-  if ! glxinfo | grep "OpenGL version string"; then
-    pm_message "This Port does not support the libMali graphics driver. Switch to Panfrost to continue."
-    sleep 5
-    exit 1
-  fi
-fi
 
 # the default pulseaudio backend doesn't always work well
 if [[ "$CFW_NAME" = "ROCKNIX" ]] || [[ "$CFW_NAME" = "AmberELEC" ]]; then
@@ -75,24 +49,8 @@ fi
 # Start game 
 pushd $DATADIR/x86-64/
 
-$GPTOKEYB "$BINARY" -c "$GAMEDIR/ratcheteerdx.gptk" &
-
-# Start Westonpack
-$ESUDO env \
-CRUSTY_BLOCK_INPUT=1 \
-SDL_AUDIODRIVER=$audio_backend \
-BOX64_LD_LIBRARY_PATH="./":"$GAMEDIR/box64/box64-x86_64-linux-gnu" \
-$weston_dir/westonwrap.sh headless noop kiosk crusty_glx_gl4es \
-XDG_CONFIG_HOME=$CONFDIR \
-XDG_DATA_HOME=$LOCALDIR \
+#$GPTOKEYB "$BINARY" -c "$GAMEDIR/ratcheteerdx.gptk" &
+$GPTOKEYB "$BINARY" &
 $GAMEDIR/box64/box64 ./gamedata/$BINARY
-
-popd
-
-# Clean up after ourselves
-$ESUDO $weston_dir/westonwrap.sh cleanup
-if [[ "$PM_CAN_MOUNT" != "N" ]]; then
-    $ESUDO umount "${weston_dir}"
-fi
 
 pm_finish
