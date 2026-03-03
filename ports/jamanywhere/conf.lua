be3b964ee1550c9646941d56851a9c8e97953611
mrozkar <80858071+mrozkar@users.noreply.github.com>
2026-03-03 21:24:27 +0100

    Delete ports/jamanywhere directory

--- ports/jamanywhere/conf.lua
+++
@@ -1,9 +0,0 @@
-function love.conf(t)
-    t.window.title = "Jam Anywhere"
-    t.window.width = 640
-    t.window.height = 480
-    t.window.fullscreen = true
-    t.window.fullscreentype = "desktop" -- Automatycznie rozciągnie grę na pełny ekran konsoli
-    t.window.vsync = 1                  -- Zapobiega "rwie" ekranu (tearing)
-    t.console = false
-end
