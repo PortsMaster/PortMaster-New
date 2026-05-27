3867578e06fb479088ec336e4a1b5c7c0dcdf53a
Fraxinus88 <157858080+Fraxinus88@users.noreply.github.com>
2026-05-27 21:07:56 +0800

    deleted swap buttons

--- ports/roguelight/Roguelight.sh
+++ ports/roguelight/Roguelight.sh
@@ -54,11 +54,6 @@ else
     pm_message "Patching process already completed. Skipping."
 fi
 
-# Swap buttons
-"$GAMEDIR/tools/SDL_swap_gpbuttons.py" -i "$SDL_GAMECONTROLLERCONFIG_FILE" -o "$GAMEDIR/gamecontrollerdb_swapped.txt" -l "$GAMEDIR/SDL_swap_gpbuttons.txt"
-export SDL_GAMECONTROLLERCONFIG_FILE="$GAMEDIR/gamecontrollerdb_swapped.txt"
-export SDL_GAMECONTROLLERCONFIG="`echo "$SDL_GAMECONTROLLERCONFIG" | "$GAMEDIR/tools/SDL_swap_gpbuttons.py" -l "$GAMEDIR/SDL_swap_gpbuttons.txt"`"
-
 # Display loading splash
 if [ -f "$GAMEDIR/patchlog.txt" ]; then
     $ESUDO ./tools/splash "splash.png" 4000 &
