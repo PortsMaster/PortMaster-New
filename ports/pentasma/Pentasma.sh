afe4acf83a4ad994fa0e89bb05c2ce52487123bc
Ganimoth <ganimoth@gmail.com>
2026-07-27 22:08:37 +0200

    New port: Pentasma

---
+++ ports/pentasma/Pentasma.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# Variables
+GAMEDIR="/$directory/ports/pentasma"
+
+# CD and set log
+cd $GAMEDIR
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+# Ensure executable permissions
+$ESUDO chmod +x "$GAMEDIR/gmloadernext.aarch64"
+$ESUDO chmod +x "$GAMEDIR/tools/patchscript"
+
+# Exports
+export LD_LIBRARY_PATH="/usr/lib:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+# Check if 7zzs present
+if [ ! -f "$controlfolder/7zzs.${DEVICE_ARCH}" ]; then
+    pm_message "this port requires the latest portmaster to run, please go to https://portmaster.games/ for more info."
+    sleep 5
+    exit 1
+fi
+
+# Check if we need to patch the game
+if [ ! -f install_completed ]; then
+    if [ -f "$controlfolder/utils/patcher.txt" ]; then
+        export PATCHER_FILE="$GAMEDIR/tools/patchscript"
+        export PATCHER_QUESTIONS="$GAMEDIR/tools/question.lua"
+        export PATCHER_GAME="Pentasma"
+        export PATCHER_TIME="just a moment"
+        export ESUDO
+        export controlfolder 
+        source "$controlfolder/utils/patcher.txt"
+        $ESUDO kill -9 $(pidof gptokeyb)
+    else
+        pm_message "This port requires the latest version of PortMaster."
+    fi
+fi
+
+# Assign gptokeyb and load the game
+$GPTOKEYB "gmloadernext.aarch64" &
+pm_platform_helper "$GAMEDIR/gmloadernext.aarch64" >/dev/null
+./gmloadernext.aarch64 -c gmloader.json
+
+# Cleanup
+pm_finish
---
+++ ports/pentasma/README.md
@@ -0,0 +1,17 @@
+## Notes
+
+Thanks to `Hijon Park` for developing this game and allowing us to host it on PortMaster. Feel free to check other great games at [Itch.io](https://hijong-park.itch.io/) or [Steam](https://store.steampowered.com/developer/HijongPark).
+
+## Controls
+
+| Button | Action |
+|--|--| 
+|DPAD / Left Stick|Movement|
+|A|Smartbomb|
+|X|Shoot|
+|R1|Special|
+|L2|Reverse|
+|R2]Thrust|
+|Start|Pause/Confirm|
+
+
