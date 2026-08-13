a73d6d92fbebef7126301883379bdc1a07f3b61d
Cebion <7762208+Cebion@users.noreply.github.com>
2026-08-13 23:55:33 +0200

    Add new Port: Alex the Allegator 3

---
+++ ports/alex3/Alex the Allegator 3.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+# RockNix's Panfrost driver has a confirmed RockNix-side memory leak
+# triggered by this game's render pattern - unplayable, crashes the device.
+if [[ "$CFW_NAME" = "ROCKNIX" ]] && glxinfo 2>/dev/null | grep -q "OpenGL version string"; then
+  pm_message "This port does not support the Panfrost graphics driver due to a RockNix bug. Switch to libMali to play."
+  sleep 5
+  exit 1
+fi
+
+GAMEDIR=/$directory/ports/alex3
+CONFDIR="$GAMEDIR/conf/"
+BINARY=alex3.${DEVICE_ARCH}
+
+mkdir -p "$GAMEDIR/conf"
+
+cd $GAMEDIR
+
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+export XDG_DATA_HOME="$CONFDIR"
+export game_libs="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+REAL_XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-/run/user/$(id -u)}
+
+weston_dir=/tmp/weston
+$ESUDO mkdir -p "${weston_dir}"
+weston_runtime="weston_pkg_0.2"
+if [ ! -f "$controlfolder/libs/${weston_runtime}.squashfs" ]; then
+  if [ ! -f "$controlfolder/harbourmaster" ]; then
+    pm_message "This port requires the latest PortMaster to run, please go to https://portmaster.games/ for more info."
+    sleep 5
+    exit 1
+  fi
+  $ESUDO $controlfolder/harbourmaster --quiet --no-check runtime_check "${weston_runtime}.squashfs"
+fi
+if [[ "$PM_CAN_MOUNT" != "N" ]]; then
+    $ESUDO umount "${weston_dir}"
+fi
+$ESUDO mount "$controlfolder/libs/${weston_runtime}.squashfs" "${weston_dir}"
+
+$GPTOKEYB "$BINARY" -c "./alex3.gptk" &
+pm_platform_helper "$GAMEDIR/$BINARY"
+
+$ESUDO env WRAPPED_LIBRARY_PATH=$game_libs DISPLAY_WIDTH=640 DISPLAY_HEIGHT=480 \
+weston_backendlib="$weston_dir/lib_aarch64/extra_wayland" \
+weston_preload="$weston_dir/lib_aarch64/extra_wayland/libwayland-egl.so.1" \
+$weston_dir/westonwrap.sh drm gl kiosk system \
+ULTRADIR=$GAMEDIR XDG_DATA_HOME=$CONFDIR XDG_RUNTIME_DIR=$REAL_XDG_RUNTIME_DIR $GAMEDIR/$BINARY
+
+$ESUDO $weston_dir/westonwrap.sh cleanup
+if [[ "$PM_CAN_MOUNT" != "N" ]]; then
+    $ESUDO umount "${weston_dir}"
+fi
+pm_finish
