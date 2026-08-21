80edc6943daf9c8b730051517dd440c3779a15a3
saitamasahil <sahilmanchanda1996@gmail.com>
2026-08-21 21:33:37 +0530

    Package game assets into archive for first-startup extraction

--- ports/2048plus/2048 Plus.sh
+++ ports/2048plus/2048 Plus.sh
@@ -32,6 +32,23 @@ bind_directories ~/.2048plus $GAMEDIR/conf/.2048plus
 export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
 export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
 
+ARCHIVE_FILE="gamedata.tar.gz"
+if [[ -f "$ARCHIVE_FILE" ]]; then
+    pm_message "Extracting game data, this can take a few minutes..."
+    if gunzip -c "$ARCHIVE_FILE" | tar xf -; then
+        pm_message "Extraction successful."
+        $ESUDO rm -f "$ARCHIVE_FILE"
+    else
+        pm_message "Error: Extraction failed."
+        sleep 5
+        exit 1
+    fi
+elif [ ! -f 'gamedata/main.lua' ]; then
+    pm_message "Error: No game data present and Archive file $ARCHIVE_FILE not found."
+    sleep 5
+    exit 1
+fi
+
 source $controlfolder/runtimes/"love_11.5"/love.txt
 
 $GPTOKEYB "$LOVE_GPTK" &
