6df43dec13e9605ceca5cd777d6b762edc38d6d1
Cebion <7762208+Cebion@users.noreply.github.com>
2026-09-11 10:10:49 +0200

    Add new Port: Blip n Blup

---
+++ ports/blipnblup/Blip n Blup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+get_controls
+
+GAMEDIR=/$directory/ports/blipnblup
+BINARY=blipnblup.${DEVICE_ARCH}
+
+cd $GAMEDIR
+
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+ARCHIVE_FILE="data.tar.gz"
+if [[ -f "$ARCHIVE_FILE" ]]; then
+    if [[ -d 'Resources/' ]]; then
+        pm_message "Removing old game data"
+        $ESUDO rm -fR 'Resources/'
+    fi
+    pm_message "Extracting game data, this can take a few minutes..."
+    if gunzip -c "$ARCHIVE_FILE" | tar xf -; then
+        pm_message "Extraction successful."
+        $ESUDO rm -f "$ARCHIVE_FILE"
+    else
+        pm_message "Error: Extraction failed."
+        sleep 5
+        exit 1
+    fi
+elif [ ! -d 'Resources/' ]; then
+    pm_message "Error: No Resources directory present and Archive file $ARCHIVE_FILE not found."
+    sleep 5
+    exit 1
+fi
+
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+$GPTOKEYB2 "blipnblup" &
+
+pm_platform_helper "$GAMEDIR/$BINARY"
+
+./$BINARY
+
+pm_finish
