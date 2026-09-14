4567534dba002b7558200c35d90c937e6e44ffd1
Fraxinus88 <157858080+Fraxinus88@users.noreply.github.com>
2026-09-14 22:17:01 +0800

    New Port Evil Egg

---
+++ ports/evilegg/evilegg/tools/questions.lua
@@ -0,0 +1,18 @@
+return {
+    {
+        id = "NINTENDO_LAYOUT",
+        question = "What is the layout of your controller's face buttons? (Nintendo: A on the right, Xbox: A on the bottom)",
+        options = {
+            {"false", "Nintendo"},
+            {"true", "Xbox"}
+        }
+    },
+    {
+        id = "ONLINE_MODE",
+        question = "Would you like to enable online leaderboards? (Enabling leaderboards will worsen performance of the game)",
+        options = {
+            {"false", "No"},
+            {"true", "Yes"}
+        }
+    }
+}
