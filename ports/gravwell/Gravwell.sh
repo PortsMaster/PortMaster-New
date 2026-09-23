b0ac5bb8e995f570b7a449e8f40681f1e85f0b3a
jimmyhat999 <lewiscarter@outlook.com>
2026-09-23 01:31:41 +0100

    Add Gravwell

---
+++ ports/gravwell/Gravwell.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+
+if [ -d "/opt/system/Tools/PortMaster/" ]; then
+  controlfolder="/opt/system/Tools/PortMaster"
+elif [ -d "/opt/tools/PortMaster/" ]; then
+  controlfolder="/opt/tools/PortMaster"
+elif [ -d "$XDG_DATA_HOME/PortMaster/" ]; then
+  controlfolder="$XDG_DATA_HOME/PortMaster"
+else
+  controlfolder="/roms/ports/PortMaster"
+fi
+
+source $controlfolder/control.txt
+source $controlfolder/device_info.txt
+[ -f "${controlfolder}/mod_${CFW_NAME}.txt" ] && source "${controlfolder}/mod_${CFW_NAME}.txt"
+
+get_controls
+
+GAMEDIR="/$directory/ports/gravwell"
+CONFDIR="$GAMEDIR/conf/"
+
+mkdir -p "$GAMEDIR/conf"
+
+cd $GAMEDIR
+
+> "$GAMEDIR/log.txt" && exec > >(tee "$GAMEDIR/log.txt") 2>&1
+
+export XDG_DATA_HOME="$CONFDIR"
+export LD_LIBRARY_PATH="$GAMEDIR/libs.${DEVICE_ARCH}:$LD_LIBRARY_PATH"
+export SDL_GAMECONTROLLERCONFIG="$sdl_controllerconfig"
+
+chmod +x ./gravwell.${DEVICE_ARCH}
+
+$GPTOKEYB "gravwell.${DEVICE_ARCH}" &
+pm_platform_helper "$GAMEDIR/gravwell.${DEVICE_ARCH}"
+./gravwell.${DEVICE_ARCH}
+
+pm_finish
---
+++ ports/gravwell/README.md
@@ -0,0 +1,19 @@
+## Notes
+
+Gravwell is made by JIMMYHAT999, who also made this port. Music by [RyanAvx](https://ryanavx.itch.io/breezys-mega-quest).
+
+## Controls
+
+| Button | Action |
+|--|--|
+|D-Pad|Move|
+|Face buttons|Fire in that direction; two neighbouring buttons fire the diagonal|
+|Right stick|Aim and fire, 8 ways|
+|L1/R1/L2/R2|Dash|
+|Start or Select|Pause, and see your haul|
+|A|Confirm|
+|B|Back / reroll|
+
+OPTIONS on the title menu holds DISPLAY (Pixel perfect / Fit / Fill), the music and sound-effect mutes, the controls list and CLEAR SAVE. All of it is remembered between sessions.
+
+On Knulli, enable the A/B X/Y swap option.
